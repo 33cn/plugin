e0a7bda6a93e445795b6f0e8ba2ecdb33d6d56bd
kingwang <kingwang@kingwangdeiMac.local>
2018-11-07 15:18:09 +0800

    init all plugin

---
+++ dapp/evm/executor/vm/model/const.go
@@ -0,0 +1,8 @@
+package model
+
+const (
+	// 内存中存储的字，占用多少位
+	WordBitSize = 256
+	// 内存中存储的字，占用多少字节
+	WordByteSize = WordBitSize / 8
+)
---
+++ dapp/token/types/const.go
@@ -0,0 +1,39 @@
+package types
+
+const (
+	//action for token
+	ActionTransfer            = 4
+	ActionGenesis             = 5
+	ActionWithdraw            = 6
+	TokenActionPreCreate      = 7
+	TokenActionFinishCreate   = 8
+	TokenActionRevokeCreate   = 9
+	TokenActionTransferToExec = 11
+)
+
+// token status
+const (
+	TokenStatusPreCreated = iota
+	TokenStatusCreated
+	TokenStatusCreateRevoked
+)
+
+var (
+	TokenX = "token"
+)
+
+const (
+	TyLogPreCreateToken       = 211
+	TyLogFinishCreateToken    = 212
+	TyLogRevokeCreateToken    = 213
+	TyLogTokenTransfer        = 313
+	TyLogTokenGenesis         = 314
+	TyLogTokenDeposit         = 315
+	TyLogTokenExecTransfer    = 316
+	TyLogTokenExecWithdraw    = 317
+	TyLogTokenExecDeposit     = 318
+	TyLogTokenExecFrozen      = 319
+	TyLogTokenExecActive      = 320
+	TyLogTokenGenesisTransfer = 321
+	TyLogTokenGenesisDeposit  = 322
+)
