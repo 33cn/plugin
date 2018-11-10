217f2cae722d2e8148cdf03ef2e715c5139f1c6b
madengji <mdj@disanbo.com>
2018-11-10 15:32:38 +0800

    plugin add ci

--- cli/main.go
+++ cli/main.go
@@ -6,8 +6,13 @@ import (
 	_ "gitlab.33.cn/chain33/chain33/system"
 	"gitlab.33.cn/chain33/chain33/util/cli"
 	_ "gitlab.33.cn/chain33/plugin/plugin"
+	"gitlab.33.cn/chain33/plugin/cli/buildflags"
 )
 
 func main() {
-	cli.Run("", "")
+	if buildflags.RPCAddr == "" {
+		buildflags.RPCAddr = "http://localhost:8801"
+	}
+	cli.Run(buildflags.RPCAddr, buildflags.ParaName)
+
 }
