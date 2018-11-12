57cffd733fcfef7043c76ea9e8bb26dd293f9ec1
king wang <kingwang@kingdeMacBook-Pro.local>
2018-11-12 21:21:25 +0800

    add some doc

--- main.go
+++ main.go
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+/*
+每个系统的功能通过插件完成，插件分成4类：
+共识 加密 dapp 存储
+这个go 包提供了 官方提供的 插件。
+*/
 // +build go1.8
 
 package main
