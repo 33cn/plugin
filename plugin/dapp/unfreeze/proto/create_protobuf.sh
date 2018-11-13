b2128aff04854688ee97bf137ff3eb3d033dfed3
linj-disanbo <linj@disanbo.com>
2018-11-13 19:31:55 +0800

    add copyright

--- plugin/dapp/unfreeze/proto/create_protobuf.sh
+++ plugin/dapp/unfreeze/proto/create_protobuf.sh
@@ -1,2 +1,6 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #!/bin/sh
 protoc --go_out=plugins=grpc:../types ./*.proto --proto_path=. --proto_path="$GOPATH/src/gitlab.33.cn/chain33/chain33/types/proto/"
