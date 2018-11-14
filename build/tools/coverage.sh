b9d591d3212c6e14abd47090756904cc0c44f281
mdj33 <mdj@disanbo.com>
2018-11-14 17:21:44 +0800

    add race test

--- build/tools/coverage.sh
+++ build/tools/coverage.sh
@@ -12,7 +12,7 @@ mkdir -p "$COVERAGE_DIR"
 
 # Create a coverage file for each package
 for package in ${PKG_LIST}; do
-    go test -covermode=count -coverprofile "${COVERAGE_DIR}/${package##*/}.cov" "$package"
+    go test -race -covermode=count -coverprofile "${COVERAGE_DIR}/${package##*/}.cov" "$package"
 done
 
 # Merge the coverage profile files
