fdb2aa2d5466386d3036e16ed026729fb503b7d9
vipwzw <vipwzw@vip.qq.com>
2018-11-26 13:38:21 +0800

    fix test

--- main.go
+++ main.go
@@ -12,7 +12,7 @@
 package main
 
 import (
-     	_ "github.com/33cn/chain33/system"
+        	_ "github.com/33cn/chain33/system"
 	"github.com/33cn/chain33/util/cli"
 	_ "github.com/33cn/plugin/plugin"
 )
