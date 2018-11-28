ed27051beb49334244e50758a9539697cb6b716b
vipwzw <vipwzw@vip.qq.com>
2018-11-29 00:24:16 +0800

    fix linter

--- plugin/init.go
+++ plugin/init.go
@@ -4,5 +4,5 @@ import (
 	_ "github.com/33cn/plugin/plugin/consensus/init" //consensus init
 	_ "github.com/33cn/plugin/plugin/crypto/init"    //crypto init
 	_ "github.com/33cn/plugin/plugin/dapp/init"      //dapp init
-	_ "github.com/33cn/plugin/plugin/store/init      //store init"
+	_ "github.com/33cn/plugin/plugin/store/init"     //store init
 )
