e274fdf82dbc690ed23453540b93d063e2c6490e
linj-disanbo <linj@disanbo.com>
2018-12-04 10:21:03 +0800

    fix

--- plugin/dapp/unfreeze/proto/create_protobuf.sh
+++ plugin/dapp/unfreeze/proto/create_protobuf.sh
@@ -1,6 +1,2 @@
-// Copyright Fuzamei Corp. 2018 All Rights Reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 #!/bin/sh
 protoc --go_out=plugins=grpc:../types ./*.proto --proto_path=. --proto_path="$GOPATH/src/github.com/33cn/chain33/types/proto/"
