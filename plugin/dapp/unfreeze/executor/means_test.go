7a7287d7ef89e6d7d040348c023fb56bdb0a9822
vipwzw <vipwzw@vip.qq.com>
2018-12-05 14:24:13 +0800

    auto ci

--- plugin/dapp/unfreeze/executor/means_test.go
+++ plugin/dapp/unfreeze/executor/means_test.go
@@ -1,8 +1,9 @@
 package executor
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	pty "github.com/33cn/plugin/plugin/dapp/unfreeze/types"
 )
