013b9f8aed97887192aa99c02d504f931e4826dd
chasingegg <gaoc96@qq.com>
2018-12-08 00:48:59 +0800

    initial commit

---
+++ plugin/consensus/pbftlibbyz/block.go
@@ -0,0 +1,143 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pbftlibbyz
+
+import (
+	"time"
+
+	"github.com/33cn/chain33/common/merkle"
+	"github.com/33cn/chain33/queue"
+	drivers "github.com/33cn/chain33/system/consensus"
+	cty "github.com/33cn/chain33/system/dapp/coins/types"
+	"github.com/33cn/chain33/types"
+)
+
+func init() {
+	drivers.Reg("pbftlibbyz", Newpbftlibbyz)
+	drivers.QueryData.Register("pbftlibbyz", &Client{})
+}
+
+// Client pbftlibbyz implementation
+type Client struct {
+	*drivers.BaseClient
+	replyChan   chan *types.ClientReply
+	requestChan chan *types.Request
+	isPrimary   bool
+}
+
+// NewBlockstore create pbftlibbyz Client
+func NewBlockstore(cfg *types.Consensus, replyChan chan *types.ClientReply, requestChan chan *types.Request, isPrimary bool) *Client {
+	c := drivers.NewBaseClient(cfg)
+	client := &Client{BaseClient: c, replyChan: replyChan, requestChan: requestChan, isPrimary: isPrimary}
+	c.SetChild(client)
+	return client
+}
+
+// ProcEvent method
+func (client *Client) ProcEvent(msg queue.Message) bool {
+	return false
+}
+
+// Propose method
+func (client *Client) Propose(block *types.Block) {
+	op := &types.Operation{Value: block}
+	req := ToRequestClient(op, types.Now().String(), clientAddr)
+	client.requestChan <- req
+}
+
+// CheckBlock method
+func (client *Client) CheckBlock(parent *types.Block, current *types.BlockDetail) error {
+	return nil
+}
+
+// SetQueueClient method
+func (client *Client) SetQueueClient(c queue.Client) {
+	plog.Info("Enter SetQueue method of pbftlibbyz consensus")
+	client.InitClient(c, func() {
+
+		client.InitBlock()
+	})
+	go client.EventLoop()
+	//go client.readReply()
+	go client.CreateBlock()
+}
+
+// CreateBlock method
+func (client *Client) CreateBlock() {
+	issleep := true
+	if !client.isPrimary {
+		return
+	}
+	for {
+		if issleep {
+			time.Sleep(10 * time.Second)
+		}
+		plog.Info("=============start get tx===============")
+		lastBlock := client.GetCurrentBlock()
+		txs := client.RequestTx(int(types.GetP(lastBlock.Height+1).MaxTxNumber), nil)
+		if len(txs) == 0 {
+			issleep = true
+			continue
+		}
+		issleep = false
+		plog.Info("==================start create new block!=====================")
+		//check dup
+		//txs = client.CheckTxDup(txs)
+		//fmt.Println(len(txs))
+
+		var newblock types.Block
+		newblock.ParentHash = lastBlock.Hash()
+		newblock.Height = lastBlock.Height + 1
+		newblock.Txs = txs
+		newblock.TxHash = merkle.CalcMerkleRoot(newblock.Txs)
+		newblock.BlockTime = types.Now().Unix()
+		if lastBlock.BlockTime >= newblock.BlockTime {
+			newblock.BlockTime = lastBlock.BlockTime + 1
+		}
+		client.Propose(&newblock)
+		//time.Sleep(time.Second)
+		client.readReply()
+		plog.Info("===============readreply and writeblock done===============")
+	}
+}
+
+// GetGenesisBlockTime get genesis blocktime
+func (client *Client) GetGenesisBlockTime() int64 {
+	return genesisBlockTime
+}
+
+// CreateGenesisTx get genesis tx
+func (client *Client) CreateGenesisTx() (ret []*types.Transaction) {
+	var tx types.Transaction
+	tx.Execer = []byte("coins")
+	tx.To = genesis
+	//gen payload
+	g := &cty.CoinsAction_Genesis{}
+	g.Genesis = &types.AssetsGenesis{}
+	g.Genesis.Amount = 1e8 * types.Coin
+	tx.Payload = types.Encode(&cty.CoinsAction{Value: g, Ty: cty.CoinsActionGenesis})
+	ret = append(ret, &tx)
+	return
+}
+
+func (client *Client) readReply() {
+
+	data := <-client.replyChan
+	if data == nil {
+		plog.Error("block is nil")
+		return
+	}
+	plog.Info("===============Get block from reply channel===========")
+	//client.SetCurrentBlock(data.Result.Value)
+	lastBlock := client.GetCurrentBlock()
+	err := client.WriteBlock(lastBlock.StateHash, data.Result.Value)
+
+	if err != nil {
+		plog.Error("********************err:", err)
+		return
+	}
+	client.SetCurrentBlock(data.Result.Value)
+
+}
---
+++ plugin/consensus/pbftlibbyz/messages.go
@@ -0,0 +1,173 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pbftlibbyz
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"net"
+
+	"github.com/33cn/chain33/types"
+	"github.com/golang/protobuf/proto"
+)
+
+// EQ Digest
+func EQ(d1 []byte, d2 []byte) bool {
+	if len(d1) != len(d2) {
+		return false
+	}
+	for idx, b := range d1 {
+		if b != d2[idx] {
+			return false
+		}
+	}
+	return true
+}
+
+// ToCheckpoint method
+func ToCheckpoint(sequence uint32, digest []byte) *types.Checkpoint {
+	return &types.Checkpoint{Sequence: sequence, Digest: digest}
+}
+
+// ToEntry method
+func ToEntry(sequence uint32, digest []byte, view uint32) *types.Entry {
+	return &types.Entry{Sequence: sequence, Digest: digest, View: view}
+}
+
+// ToViewChange method
+func ToViewChange(viewchanger uint32, digest []byte) *types.ViewChange {
+	return &types.ViewChange{Viewchanger: viewchanger, Digest: digest}
+}
+
+// ToSummary method
+func ToSummary(sequence uint32, digest []byte) *types.Summary {
+	return &types.Summary{Sequence: sequence, Digest: digest}
+}
+
+// ToRequestClient method
+func ToRequestClient(op *types.Operation, timestamp, client string) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Client{
+			Client: &types.RequestClient{Op: op, Timestamp: timestamp, Client: client}},
+	}
+}
+
+// ToRequestPreprepare method
+func ToRequestPreprepare(view, sequence uint32, digest []byte, replica uint32) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Preprepare{
+			Preprepare: &types.RequestPrePrepare{View: view, Sequence: sequence, Digest: digest, Replica: replica}},
+	}
+}
+
+// ToRequestPrepare method
+func ToRequestPrepare(view, sequence uint32, digest []byte, replica uint32) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Prepare{
+			Prepare: &types.RequestPrepare{View: view, Sequence: sequence, Digest: digest, Replica: replica}},
+	}
+}
+
+// ToRequestCommit method
+func ToRequestCommit(view, sequence, replica uint32) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Commit{
+			Commit: &types.RequestCommit{View: view, Sequence: sequence, Replica: replica}},
+	}
+}
+
+// ToRequestCheckpoint method
+func ToRequestCheckpoint(sequence uint32, digest []byte, replica uint32) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Checkpoint{
+			Checkpoint: &types.RequestCheckpoint{Sequence: sequence, Digest: digest, Replica: replica}},
+	}
+}
+
+// ToRequestViewChange method
+func ToRequestViewChange(view, sequence uint32, checkpoints []*types.Checkpoint, preps, prePreps []*types.Entry, replica uint32) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Viewchange{
+			Viewchange: &types.RequestViewChange{View: view, Sequence: sequence, Checkpoints: checkpoints, Preps: preps, Prepreps: prePreps, Replica: replica}},
+	}
+}
+
+// ToRequestAck method
+func ToRequestAck(view, replica, viewchanger uint32, digest []byte) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Ack{
+			Ack: &types.RequestAck{View: view, Replica: replica, Viewchanger: viewchanger, Digest: digest}},
+	}
+}
+
+// ToRequestNewView method
+func ToRequestNewView(view uint32, viewChanges []*types.ViewChange, summaries []*types.Summary, replica uint32) *types.Request {
+	return &types.Request{
+		Value: &types.Request_Newview{
+			Newview: &types.RequestNewView{View: view, Viewchanges: viewChanges, Summaries: summaries, Replica: replica}},
+	}
+}
+
+// ReqDigest method
+func ReqDigest(req *types.Request) []byte {
+	if req == nil {
+		return nil
+	}
+	bytes := md5.Sum([]byte(req.String()))
+	return bytes[:]
+}
+
+/*func (req *Request) LowWaterMark() uint32 {
+	// only for requestViewChange
+	reqViewChange := req.GetViewchange()
+	checkpoints := reqViewChange.GetCheckpoints()
+	lastStable := checkpoints[len(checkpoints)-1]
+	lwm := lastStable.Sequence
+	return lwm
+}*/
+
+// ToReply method
+func ToReply(view uint32, timestamp, client string, replica uint32, result *types.Result) *types.ClientReply {
+	return &types.ClientReply{View: view, Timestamp: timestamp, Client: client, Replica: replica, Result: result}
+}
+
+// RepDigest method
+func RepDigest(reply fmt.Stringer) []byte {
+	if reply == nil {
+		return nil
+	}
+	bytes := md5.Sum([]byte(reply.String()))
+	return bytes[:]
+}
+
+// WriteMessage write proto message
+func WriteMessage(addr string, msg proto.Message) error {
+	conn, err := net.Dial("tcp", addr)
+	defer conn.Close()
+	if err != nil {
+		return err
+	}
+	bz, err := proto.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	n, err := conn.Write(bz)
+	plog.Debug("size of byte is", "", n)
+	return err
+}
+
+// ReadMessage read proto message
+func ReadMessage(conn io.Reader, msg proto.Message) error {
+	var buf bytes.Buffer
+	n, err := io.Copy(&buf, conn)
+	plog.Debug("size of byte is", "", n)
+	if err != nil {
+		return err
+	}
+	err = proto.Unmarshal(buf.Bytes(), msg)
+	return err
+}
