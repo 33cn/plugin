eaaeeb54c9c1bf13f74b8ca8866a8cc7ef9bd5d2
vipwzw <vipwzw@vip.qq.com>
2018-12-19 11:16:52 +0800

    update chain33

--- plugin/store/init/init.go
+++ plugin/store/init/init.go
@@ -1,7 +1,1 @@
 package init
-
-import (
-	_ "github.com/33cn/plugin/plugin/store/kvdb"   //auto gen
-	_ "github.com/33cn/plugin/plugin/store/kvmvcc" //auto gen
-	_ "github.com/33cn/plugin/plugin/store/mpt"    //auto gen
-)
