80a973559c4892cd3d79a278d16e65f6cf5c3682
vipwzw <vipwzw@vip.qq.com>
2018-12-19 11:42:06 +0800

    Update init.go

--- plugin/consensus/init/init.go
+++ plugin/consensus/init/init.go
@@ -1,1 +1,9 @@
 package init
+
+import (
+	_ "github.com/33cn/plugin/plugin/consensus/para"       //auto gen
+	_ "github.com/33cn/plugin/plugin/consensus/pbft"       //auto gen
+	_ "github.com/33cn/plugin/plugin/consensus/raft"       //auto gen
+	_ "github.com/33cn/plugin/plugin/consensus/tendermint" //auto gen
+	_ "github.com/33cn/plugin/plugin/consensus/ticket"     //auto gen
+)
