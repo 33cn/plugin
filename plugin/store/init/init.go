63567b2ae77e16962e24090bf67e7cbdd01146b5
vipwzw <vipwzw@vip.qq.com>
2018-12-19 11:43:07 +0800

    Update init.go

--- plugin/store/init/init.go
+++ plugin/store/init/init.go
@@ -1,1 +1,7 @@
 package init
+
+import (
+	_ "github.com/33cn/plugin/plugin/store/kvdb"   //auto gen
+	_ "github.com/33cn/plugin/plugin/store/kvmvcc" //auto gen
+	_ "github.com/33cn/plugin/plugin/store/mpt"    //auto gen
+)
