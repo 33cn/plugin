01b150b44b6d7f667bf5db43de579a755972c0a2
linj-disanbo <linj@disanbo.com>
2018-12-21 14:52:08 +0800

    client support token send to exec

--- plugin/dapp/token/commands/utils.go
+++ plugin/dapp/token/commands/utils.go
@@ -6,8 +6,9 @@ package commands
 
 import (
 	"encoding/hex"
-
+	"fmt"
 	"math"
+	"os"
 	"strings"
 
 	"github.com/33cn/chain33/common/address"
@@ -17,22 +18,38 @@ import (
 )
 
 // CreateRawTx 创建交易
+// 主链交易使用 Transaction 的 To
+// 平行链使用的 payload 的 To， Transaction 的 To 作为执行器的合约地址
+// 在执行合约时， 有GetRealTo 用来读取这个项
+// 在命令行中， 参数需要加前缀才是对的
+// 在平行链上 执行器地址注册不带前缀， withdraw To地址有前缀， 算出来不一致
 func CreateRawTx(cmd *cobra.Command, to string, amount float64, note string, isWithdraw bool, tokenSymbol, execName string) (string, error) {
 	if amount < 0 {
 		return "", types.ErrAmount
 	}
 	paraName, _ := cmd.Flags().GetString("paraName")
 	amountInt64 := int64(math.Trunc((amount+0.0000001)*1e4)) * 1e4
-	execName = getRealExecName(paraName, execName)
-	if execName != "" && !types.IsAllowExecName([]byte(execName), []byte(execName)) {
-		return "", types.ErrExecNameNotMatch
-	}
 	var tx *types.Transaction
 	transfer := &tokenty.TokenAction{}
 	if isWithdraw {
-		v := &tokenty.TokenAction_Withdraw{Withdraw: &types.AssetsWithdraw{Cointoken: tokenSymbol, Amount: amountInt64, Note: []byte(note), To: to}}
+		v := &tokenty.TokenAction_Withdraw{Withdraw: &types.AssetsWithdraw{Cointoken: tokenSymbol, Amount: amountInt64,
+			Note: []byte(note), To: to, ExecName: getRealExecName(paraName, execName)}}
 		transfer.Value = v
 		transfer.Ty = tokenty.ActionWithdraw
+	} else if execName != "" {
+		execName = getRealExecName(paraName, execName)
+		if execName != "" && !types.IsAllowExecName([]byte(execName), []byte(execName)) {
+			return "", types.ErrExecNameNotMatch
+		}
+		to, err := GetExecAddr(execName)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return "", err
+		}
+		v := &tokenty.TokenAction_TransferToExec{TransferToExec: &types.AssetsTransferToExec{Cointoken: tokenSymbol,
+			Amount: amountInt64, Note: []byte(note), ExecName: execName, To: to}}
+		transfer.Value = v
+		transfer.Ty = tokenty.TokenActionTransferToExec
 	} else {
 		v := &tokenty.TokenAction_Transfer{Transfer: &types.AssetsTransfer{Cointoken: tokenSymbol, Amount: amountInt64, Note: []byte(note), To: to}}
 		transfer.Value = v
