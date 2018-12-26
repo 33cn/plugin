5c6bc19633c106556306441bba0f4c8612e6bb10
harry <lhl@disanbo.com>
2018-12-26 11:10:37 +0800

    ajust name for isCaughtUp

--- plugin/consensus/para/paracommitmsg.go
+++ plugin/consensus/para/paracommitmsg.go
@@ -83,7 +83,7 @@ out:
 			}
 
 		case block := <-client.mainBlockAdd:
-			if client.currentTx != nil && client.paraClient.isCaughtup {
+			if client.currentTx != nil && client.paraClient.isCaughtUp {
 				exist := checkTxInMainBlock(client.currentTx, block)
 				if exist {
 					finishHeight = sendingHeight
