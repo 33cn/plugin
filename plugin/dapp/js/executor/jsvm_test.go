895976f2ccd0cdb046b81cc467fa053902c737bc
vipwzw <vipwzw@vip.qq.com>
2019-01-02 13:11:45 +0800

    测试cache的内存花费

--- plugin/dapp/js/executor/jsvm_test.go
+++ plugin/dapp/js/executor/jsvm_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
+	"runtime"
 	"strings"
 	"testing"
 	"time"
@@ -229,4 +230,30 @@ func TestCalcLocalPrefix(t *testing.T) {
 	assert.Equal(t, calcStatePrefix([]byte("a")), []byte("mavl-a-"))
 	assert.Equal(t, calcCodeKey("a"), []byte("mavl-js-code-a"))
 	assert.Equal(t, calcRollbackKey([]byte("a")), []byte("LODB-js-rollback-a"))
+}
+
+func TestCacheMemUsage(t *testing.T) {
+	dir, ldb, kvdb := util.CreateTestDB()
+	defer util.CloseTestDB(dir, ldb)
+	e := initExec(ldb, kvdb, t)
+	vm, err := e.createVM("test", nil, 0)
+	assert.Nil(t, err)
+	vms := make([]*otto.Otto, 1024)
+	for i := 0; i < 1024; i++ {
+		vms[i] = vm.Copy()
+	}
+	printMemUsage()
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
