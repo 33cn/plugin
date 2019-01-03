5a633ba3c5bed17809edda0329c075169d8e31b6
vipwzw <vipwzw@vip.qq.com>
2019-01-03 18:38:50 +0800

    fix linter

--- plugin/dapp/js/executor/gen.sh
+++ plugin/dapp/js/executor/gen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-printf "package executor\n\nvar callcode = \`\n" >const.go
-cat "runtime.js" >>const.go
-printf '`\n' >>const.go
-printf 'var jscode = `\n' >>const.go
-cat "test.js" >>const.go
-printf '`\n' >>const.go
-printf "var _ = jscode\n" >>const.go
+{
+    printf 'package executor\n\nvar callcode = `\n' 
+    cat "runtime.js"
+    printf '`\n'
+    printf 'var jscode = `\n'
+    cat "test.js"
+    printf '`\n'
+    printf 'var _ = jscode\n'
+} > const.go
