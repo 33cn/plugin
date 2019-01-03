5edf1fd36a403db2c7e9074dda4e58774775c038
vipwzw <vipwzw@vip.qq.com>
2019-01-03 17:15:59 +0800

    Update gen.sh

--- plugin/dapp/js/executor/gen.sh
+++ plugin/dapp/js/executor/gen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "package executor\n\nvar callcode =  \`" >const.go
+echo "package executor\n\nvar callcode = \`" >const.go
 cat runtime.js >>const.go
 echo '`' >>const.go
 echo 'var jscode = `' >>const.go
