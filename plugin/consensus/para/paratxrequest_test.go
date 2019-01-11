e2417be25f09e2bc8ae9732e1bf82f66cae40a2c
mdj33 <travis@travis-ci.org>
2019-01-11 18:44:18 +0800

    add getblockbySeq interface and ut

--- plugin/consensus/para/paratxrequest_test.go
+++ plugin/consensus/para/paratxrequest_test.go
@@ -6,7 +6,7 @@ package para
 
 import (
 	"testing"
-
+	 "errors"
 	"github.com/33cn/chain33/blockchain"
 	"github.com/33cn/chain33/common/log"
 	"github.com/33cn/chain33/executor"
@@ -14,9 +14,6 @@ import (
 	"github.com/33cn/chain33/p2p"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
-
-	//"github.com/33cn/plugin/plugin/dapp/paracross/rpc"
-	"time"
 
 	"github.com/33cn/chain33/queue"
 	"github.com/33cn/chain33/store"
@@ -28,7 +25,7 @@ import (
 
 func init() {
 	//types.Init("user.p.para.", nil)
-	log.SetLogLevel("debug")
+	log.SetLogLevel("error")
 }
 
 type suiteParaClient struct {
@@ -43,6 +40,8 @@ type suiteParaClient struct {
 	mem     queue.Module
 	network *p2p.P2p
 }
+
+
 
 func (s *suiteParaClient) initEnv(cfg *types.Config, sub *types.ConfigSubModule) {
 	q := queue.New("channel")
@@ -59,42 +58,15 @@ func (s *suiteParaClient) initEnv(cfg *types.Config, sub *types.ConfigSubModule)
 	s.store.SetQueueClient(q.Client())
 
 	//cfg.Consensus.StartHeight = 0
-	cfg.Consensus.EmptyBlockInterval = 1
+	//add block by UT below
+	cfg.Consensus.EmptyBlockInterval = 100
+
 	s.para = New(cfg.Consensus, sub.Consensus["para"]).(*client)
 	s.grpcCli = &typesmocks.Chain33Client{}
-	blockHash := &types.BlockSequence{
-		Hash: []byte("1"),
-		Type: 1,
-	}
-	blockSeqs := &types.BlockSequences{Items: []*types.BlockSequence{blockHash}}
 
-	s.grpcCli.On("GetBlockSequences", mock.Anything, mock.Anything).Return(blockSeqs, nil)
-	block := &types.Block{Height: 0}
-	blockDetail := &types.BlockDetail{Block: block}
-	blockDetails := &types.BlockDetails{Items: []*types.BlockDetail{blockDetail}}
-	s.grpcCli.On("GetBlockByHashes", mock.Anything, mock.Anything).Return(blockDetails, nil).Once()
-	block = &types.Block{Height: 6, BlockTime: 8888888888}
-	blockDetail = &types.BlockDetail{Block: block}
-	blockDetails = &types.BlockDetails{Items: []*types.BlockDetail{blockDetail}}
-	s.grpcCli.On("GetBlockByHashes", mock.Anything, mock.Anything).Return(blockDetails, nil).Once()
-	block = &types.Block{Height: 0}
-	blockDetail = &types.BlockDetail{Block: block}
-	blockDetails = &types.BlockDetails{Items: []*types.BlockDetail{blockDetail}}
-	s.grpcCli.On("GetBlockByHashes", mock.Anything, mock.Anything).Return(blockDetails, nil).Once()
-	block = &types.Block{Height: 0}
-	blockDetail = &types.BlockDetail{Block: block}
-	blockDetails = &types.BlockDetails{Items: []*types.BlockDetail{blockDetail}}
-	s.grpcCli.On("GetBlockByHashes", mock.Anything, mock.Anything).Return(blockDetails, nil)
 
-	seq := &types.Int64{Data: 1}
-	s.grpcCli.On("GetLastBlockSequence", mock.Anything, mock.Anything).Return(seq, nil).Once()
-	seq = &types.Int64{Data: 2}
-	s.grpcCli.On("GetLastBlockSequence", mock.Anything, mock.Anything).Return(seq, nil).Once()
-	seq = &types.Int64{Data: 3}
-	s.grpcCli.On("GetLastBlockSequence", mock.Anything, mock.Anything).Return(seq, nil)
+	s.createBlockMock()
 
-	seq = &types.Int64{Data: 1}
-	s.grpcCli.On("GetSequenceByHash", mock.Anything, mock.Anything).Return(seq, nil)
 
 	reply := &types.Reply{IsOk: true}
 	s.grpcCli.On("IsSync", mock.Anything, mock.Anything).Return(reply, nil)
@@ -115,52 +87,74 @@ func (s *suiteParaClient) initEnv(cfg *types.Config, sub *types.ConfigSubModule)
 	s.network = p2p.New(cfg.P2P)
 	s.network.SetQueueClient(q.Client())
 
+	//create block self
+	s.createBlock()
 }
 
-func (s *suiteParaClient) TestRun_Test() {
-	//s.testGetBlock()
-	lastBlock, err := s.para.RequestLastBlock()
-	if err != nil {
-		plog.Error("para test", "err", err.Error())
+func (s *suiteParaClient) createBlockMock() {
+	var i, hashdata int64
+	for i=0; i<3;i++{
+		hashdata = i
+		if i>0{
+			hashdata = i-1
+		}
+
+		block := &types.Block{
+			Height:     i,
+			ParentHash: []byte(string(hashdata)),
+		}
+		blockSeq := &types.BlockSeq{
+			Seq: &types.BlockSequence{
+				Hash: []byte(string(i)),
+				Type: 1,
+			},
+			Detail: &types.BlockDetail{Block: block},
+		}
+
+		s.grpcCli.On("GetBlockBySeq", mock.Anything, &types.Int64{Data: i}).Return(blockSeq, nil)
 	}
-	plog.Info("para test---------1", "last height", lastBlock.Height)
-	s.para.createBlock(lastBlock, nil, 0, getMainBlock(2, lastBlock.BlockTime+1))
-	lastBlock, err = s.para.RequestLastBlock()
-	if err != nil {
-		plog.Error("para test--2", "err", err.Error())
+
+	// set block 3's parentHasn not equal, enter switch
+	block3 := &types.Block{
+		Height:     3,
+		ParentHash: []byte(string(1)),
 	}
-	plog.Info("para test---------", "last height", lastBlock.Height)
-	s.para.createBlock(lastBlock, nil, 1, getMainBlock(3, lastBlock.BlockTime+1))
-	time.Sleep(time.Second * 1)
+	blockSeq3 := &types.BlockSeq{
+		Seq: &types.BlockSequence{
+			Hash: []byte(string(3)),
+			Type: 1,
+		},
+		Detail: &types.BlockDetail{Block: block3},
+	}
+	s.grpcCli.On("GetBlockBySeq", mock.Anything, &types.Int64{Data: 3}).Return(blockSeq3, nil)
 
-	s.testRunGetMinerTxInfo()
-	s.testRunRmvBlock()
+	// RequestTx GetLastSeqOnMainChain
+	seq := &types.Int64{Data: 1}
+	s.grpcCli.On("GetLastBlockSequence", mock.Anything, mock.Anything).Return(seq, nil).Once()
+	seq = &types.Int64{Data: 2}
+	s.grpcCli.On("GetLastBlockSequence", mock.Anything, mock.Anything).Return(seq, nil).Once()
+	seq = &types.Int64{Data: 3}
+	s.grpcCli.On("GetLastBlockSequence", mock.Anything, mock.Anything).Return(seq, nil)
 
+	// mock for switchHashMatchedBlock
+	s.grpcCli.On("GetSequenceByHash", mock.Anything, &types.ReqHash{Hash: []byte(string(3))}).Return(nil, errors.New("hash err")).Once()
+	s.grpcCli.On("GetSequenceByHash", mock.Anything, &types.ReqHash{Hash: []byte(string(2))}).Return(nil, errors.New("hash err")).Once()
+
+	// mock for removeBlocks
+	seq = &types.Int64{Data: 1}
+	s.grpcCli.On("GetSequenceByHash", mock.Anything, mock.Anything).Return(seq, nil)
 }
 
-func (s *suiteParaClient) testRunGetMinerTxInfo() {
-	lastBlock, err := s.para.RequestLastBlock()
-	s.Nil(err)
-	plog.Info("para test testRunGetMinerTxInfo", "last height", lastBlock.Height)
-	s.True(lastBlock.Height > 1)
-	status, err := getMinerTxInfo(lastBlock)
-	s.Nil(err)
-	s.Equal(int64(3), status.MainBlockHeight)
-
-}
-
-func (s *suiteParaClient) testRunRmvBlock() {
-	lastBlock, err := s.para.RequestLastBlock()
-	s.Nil(err)
-	plog.Info("para test testRunGetMinerTxInfo", "last height", lastBlock.Height)
-	s.True(lastBlock.Height > 1)
-	s.para.removeBlocks(1)
-
-	lastBlock, err = s.para.RequestLastBlock()
-	s.Nil(err)
-	plog.Info("para test testRunGetMinerTxInfo", "last height", lastBlock.Height)
-	s.Equal(int64(1), lastBlock.Height)
-
+func (s *suiteParaClient) createBlock() {
+	var i int64
+	for i = 0; i < 3; i++ {
+		lastBlock, err := s.para.RequestLastBlock()
+		if err != nil {
+			plog.Error("para test", "err", err.Error())
+		}
+		plog.Info("para test---------1", "last height", lastBlock.Height)
+		s.para.createBlock(lastBlock, nil, i, getMainBlock(i+1, lastBlock.BlockTime+1))
+	}
 }
 
 func (s *suiteParaClient) SetupSuite() {
@@ -173,7 +167,7 @@ func TestRunSuiteParaClient(t *testing.T) {
 }
 
 func (s *suiteParaClient) TearDownSuite() {
-	time.Sleep(time.Second * 5)
+	//time.Sleep(time.Second * 2)
 	s.block.Close()
 	s.para.Close()
 	s.network.Close()
