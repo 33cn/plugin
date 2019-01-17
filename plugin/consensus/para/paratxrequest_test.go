81f69591ff83b290ee2c350d7d207e33abc38bd6
mdj33 <travis@travis-ci.org>
2019-01-17 16:10:36 +0800

    adjust fork height init

--- plugin/consensus/para/paratxrequest_test.go
+++ plugin/consensus/para/paratxrequest_test.go
@@ -66,6 +66,7 @@ func (s *suiteParaClient) initEnv(cfg *types.Config, sub *types.ConfigSubModule)
 	s.para = New(cfg.Consensus, sub.Consensus["para"]).(*client)
 	s.grpcCli = &typesmocks.Chain33Client{}
 
+	s.grpcCli.On("GetFork", mock.Anything, &types.ReqKey{Key: []byte("ForkBlockHash")}).Return(&types.Int64{Data: 1}, nil)
 	s.createBlockMock()
 
 	reply := &types.Reply{IsOk: true}
