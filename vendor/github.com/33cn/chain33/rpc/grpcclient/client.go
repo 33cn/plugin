3603d6d7612b50d631637dc9505b5239f02e4353
kingwang <kingwang@kingwangdeiMac.local>
2019-01-24 18:31:13 +0800

    update 01/24

--- vendor/github.com/33cn/chain33/rpc/grpcclient/client.go
+++ vendor/github.com/33cn/chain33/rpc/grpcclient/client.go
@@ -5,6 +5,9 @@ import (
 	"google.golang.org/grpc"
 )
 
+// paraChainGrpcRecSize 平行链receive最大100M
+const paraChainGrpcRecSize = 100 * 1024 * 1024
+
 //NewMainChainClient 创建一个平行链的 主链 grpc chain33 客户端
 func NewMainChainClient(grpcaddr string) (types.Chain33Client, error) {
 	paraRemoteGrpcClient := types.Conf("config.consensus.sub.para").GStr("ParaRemoteGrpcClient")
@@ -12,9 +15,11 @@ func NewMainChainClient(grpcaddr string) (types.Chain33Client, error) {
 		paraRemoteGrpcClient = grpcaddr
 	}
 	if paraRemoteGrpcClient == "" {
-		paraRemoteGrpcClient = "127.0.0.1:8002"
+		paraRemoteGrpcClient = "127.0.0.1:8802"
 	}
-	conn, err := grpc.Dial(NewMultipleURL(paraRemoteGrpcClient), grpc.WithInsecure())
+
+	conn, err := grpc.Dial(NewMultipleURL(paraRemoteGrpcClient), grpc.WithInsecure(),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(paraChainGrpcRecSize)))
 	if err != nil {
 		return nil, err
 	}
