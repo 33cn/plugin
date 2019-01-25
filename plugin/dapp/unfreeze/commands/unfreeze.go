3ffe01ad008443b2efcf44c24736f98a57ee8c24
linj-disanbo <linj@disanbo.com>
2019-01-25 19:56:53 +0800

    fix commands

--- plugin/dapp/unfreeze/commands/unfreeze.go
+++ plugin/dapp/unfreeze/commands/unfreeze.go
@@ -357,7 +357,7 @@ func listUnfreezeCmd() *cobra.Command {
 	}
 	cmd.Flags().StringP("last_key", "l", "", "last key")
 	cmd.Flags().Int32P("count", "", 10, "list count")
-	cmd.Flags().Int32P("direction", "d", 1, "list direction: 0/1")
+	cmd.Flags().Int32P("direction", "d", 0, "list direction: 0/1")
 
 
 	cmd.Flags().StringP("create", "c", "", "list by creator")
@@ -381,13 +381,13 @@ func listUnfreeze(cmd *cobra.Command, args []string) {
 		funcName = "ListUnfreezeByCreator"
 	}
 
-	direction, _ :=  cmd.Flags().GetInt("direction")
-	count, _ :=  cmd.Flags().GetInt("count")
+	direction, err :=  cmd.Flags().GetInt32("direction")
+	count, _ :=  cmd.Flags().GetInt32("count")
 	last_key, _ :=  cmd.Flags().GetString("last_key")
 
 	req := &pty.ReqUnfreezes{
-		Direction:           int32( direction),
-		Count:                int32(count),
+		Direction:           direction,
+		Count:                count,
 		FromKey:              last_key,
 		Initiator:            create,
 		Beneficiary:          beneficiary,
