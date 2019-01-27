ab3313c8861b56256b6dfffb8754cea32efe2ebb
vipwzw <vipwzw@vip.qq.com>
2019-01-27 12:20:26 +0800

    Update type.go

--- plugin/dapp/paracross/types/type.go
+++ plugin/dapp/paracross/types/type.go
@@ -23,7 +23,7 @@ func init() {
 	types.AllowUserExec = append(types.AllowUserExec, []byte(ParaX))
 	types.RegistorExecutor(ParaX, NewType())
 	types.RegisterDappFork(ParaX, "Enable", 0)
-	types.RegisterDappFork(ParaX, "ForkParacrossWithdrawFromParachain", 1500000)
+	types.RegisterDappFork(ParaX, "ForkParacrossWithdrawFromParachain", 1298600)
 }
 
 // GetExecName get para exec name
