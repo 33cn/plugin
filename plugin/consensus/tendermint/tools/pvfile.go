48a6b0aeb9c2b68cfe35a3668e955f23fd8619bc
vipwzw <vipwzw@vip.qq.com>
2019-02-18 16:05:09 +0800

    auto ci

--- plugin/consensus/tendermint/tools/pvfile.go
+++ plugin/consensus/tendermint/tools/pvfile.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/33cn/chain33/common/crypto"
 	"github.com/33cn/chain33/types"
 	ttypes "github.com/33cn/plugin/plugin/consensus/tendermint/types"
@@ -13,9 +14,9 @@ import (
 
 var (
 	// tendermintlog = log15.New("module", "tendermint")
-	strChars      = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" // 62 characters
-	genFile       = "genesis.json"
-	pvFile        = "priv_validator_"
+	strChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" // 62 characters
+	genFile  = "genesis.json"
+	pvFile   = "priv_validator_"
 )
 
 func RandStr(length int) string {
@@ -25,7 +26,7 @@ MAIN_LOOP:
 		val := rand.Int63()
 		for i := 0; i < 10; i++ {
 			v := int(val & 0x3f) // rightmost 6 bits
-			if v >= 62 { // only 62 characters in strChars
+			if v >= 62 {         // only 62 characters in strChars
 				val >>= 6
 				continue
 			} else {
