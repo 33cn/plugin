51335caa9ce4d336e6eefe39511b5c5132d77662
liuyuhang <liuyuhang203@sina.com>
2019-04-15 11:19:19 +0800

    add check the mavl data is have prune

--- plugin/store/kvmvccmavl/mavl.go
+++ plugin/store/kvmvccmavl/mavl.go
@@ -14,6 +14,11 @@ import (
 	"github.com/33cn/chain33/types"
 )
 
+const (
+	// 同store/mavl中定义保持一致，即裁剪的加前缀
+	leafNodePrefix = "_mb_"
+)
+
 // MavlStore mavl store struct
 type MavlStore struct {
 	db               dbm.DB
