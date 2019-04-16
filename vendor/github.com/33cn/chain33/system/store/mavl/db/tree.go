dc6803e4a40fc57e2fe7149048beceac767e8195
liuyuhang <liuyuhang203@sina.com>
2019-04-17 01:56:16 +0800

    modify fmt

--- vendor/github.com/33cn/chain33/system/store/mavl/db/tree.go
+++ vendor/github.com/33cn/chain33/system/store/mavl/db/tree.go
@@ -661,7 +661,7 @@ func updateLocalMemTree(t *Tree, node *Node) {
 		memN.data[0] = node.leftHash
 		memN.data[1] = node.rightHash
 		memN.data[2] = node.key
-		if(isTkCloseNode) {
+		if isTkCloseNode {
 			t.tkCloseNode[uintkey(farm.Hash64(node.hash))] = memN
 		} else {
 			t.updateNode[uintkey(farm.Hash64(node.hash))] = memN
