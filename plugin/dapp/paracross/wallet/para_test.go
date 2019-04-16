06919989a4666e0158da071dbd334022fba7d377
heyubin <hyb@disanbo.com>
2019-04-16 15:19:11 +0800

    add by hyb for para-wallet

---
+++ plugin/dapp/paracross/wallet/para_test.go
@@ -0,0 +1,41 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package wallet_test
+
+import (
+	"testing"
+
+	"github.com/33cn/chain33/util"
+
+	_ "github.com/33cn/chain33/system"
+	"github.com/33cn/chain33/types"
+	_ "github.com/33cn/plugin/plugin"
+	node "github.com/33cn/plugin/plugin/dapp/paracross/testnode"
+)
+
+func TestParaQuery(t *testing.T) {
+	para := node.NewParaNode(nil, nil)
+	defer para.Close()
+
+	var param types.ReqWalletImportPrivkey
+	param.Label = "Importprivkey"
+	param.Privkey = "CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944"
+	para.Para.GetAPI().WalletImportprivkey(&param)
+
+	var param1 types.ReqNewAccount
+	param1.Label = "NewAccount"
+	para.Para.GetAPI().NewAccount(&param1)
+
+	para.Para.GetAPI().WalletLock()
+
+	//通过rpc 发生信息
+	tx := util.CreateTxWithExecer(para.Para.GetGenesisKey(), "user.p.guodun.none")
+	para.Para.SendTxRPC(tx)
+	para.Para.WaitHeight(1)
+	tx = util.CreateTxWithExecer(para.Para.GetGenesisKey(), "user.p.guodun.none")
+	para.Para.SendTxRPC(tx)
+	para.Para.WaitHeight(2)
+
+}
