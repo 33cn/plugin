6c32070faec45e141c2538bca501742f887f0b7c
vipwzw <vipwzw@vip.qq.com>
2019-04-25 17:09:00 +0800

    auto ci

--- plugin/mempool/para/base.go
+++ plugin/mempool/para/base.go
@@ -5,11 +5,12 @@ import (
 	"context"
 	"fmt"
 
+	"sync"
+
 	log "github.com/33cn/chain33/common/log/log15"
 	"github.com/33cn/chain33/queue"
 	"github.com/33cn/chain33/rpc/grpcclient"
 	"github.com/33cn/chain33/types"
-	"sync"
 )
 
 var mlog = log.New("module", "mempool.para")
