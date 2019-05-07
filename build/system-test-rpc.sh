49b19fddfb243811916b01848255155676d4a2ff
mdj33 <travis@travis-ci.org>
2019-05-07 16:21:29 +0800

    add exit if rpc test fail

--- build/system-test-rpc.sh
+++ build/system-test-rpc.sh
@@ -2,15 +2,21 @@
 
 MAIN_HTTP=""
 
+MAIN_HTTP=""
+CASE_ERR=""
+
 # $2=0 means true, other false
 echo_rst() {
     if [ "$2" -eq 0 ]; then
         echo "$1 ok"
     else
         echo "$1 err"
+        CASE_ERR="err"
     fi
 
 }
+
+
 chain33_lock() {
     ok=$(curl -s --data-binary '{"jsonrpc":"2.0","id":2,"method":"Chain33.Lock","params":[]}' -H 'content-type:text/plain;' ${MAIN_HTTP} | jq -r ".result.isOK")
     [ "$ok" == true ]
@@ -32,6 +38,11 @@ function system_test_rpc() {
     echo "ip=$MAIN_HTTP"
     chain33_lock
     chain33_unlock
+
+    if [ -n "$CASE_ERR" ]; then
+        echo "there some case error"
+        exit 1
+    fi
 }
 
 #system_rpc_test
