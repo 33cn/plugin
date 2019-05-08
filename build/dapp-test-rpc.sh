db9f928e7927677754e60d08aa2605a60daf5e17
vipwzw <vipwzw@vip.qq.com>
2019-05-08 10:12:31 +0000

    Update dapp-test-rpc.sh

--- build/dapp-test-rpc.sh
+++ build/dapp-test-rpc.sh
@@ -5,7 +5,7 @@ RPC_TESTFILE=test-rpc.sh
 
 function dapp_test_rpc() {
     local ip=$1
-    echo "=========== # dapp rpc test ============="
+    echo "============ # dapp rpc test ============="
     if [ -d dapptest ]; then
         cd dapptest || return
         dir=$(find . -maxdepth 1 -type d ! -name dapptest ! -name . | sed 's/^\.\///')
