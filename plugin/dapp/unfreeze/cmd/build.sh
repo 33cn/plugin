5ca08dd7e612ba9442f46af58056909cd50c773f
linj-disanbo <linj@disanbo.com>
2019-05-18 16:22:35 +0800

    update

--- plugin/dapp/unfreeze/cmd/build.sh
+++ plugin/dapp/unfreeze/cmd/build.sh
@@ -10,3 +10,8 @@ OUT_DIR="${output_dir}/$strapp"
 
 # shellcheck disable=SC2086
 cp ./build/* "${OUT_DIR}"
+
+
+OUT_TESTDIR="${1}/dapptest/$strapp"
+mkdir -p "${OUT_TESTDIR}"
+cp ./build/test-rpc.sh "${OUT_TESTDIR}"
