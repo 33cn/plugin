8894ea9cc3eb30ed1af5f91f8ec14e656da5575c
harry <lhl@disanbo.com>
2019-05-28 12:22:34 +0800

    add game rpc test

---
+++ plugin/dapp/game/cmd/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+strpwd=$(pwd)
+strcmd=${strpwd##*dapp/}
+strapp=${strcmd%/cmd*}
+
+OUT_DIR="${1}/$strapp"
+OUT_TESTDIR="${1}/dapptest/$strapp"
+
+PARACLI="${OUT_DIR}/chain33-para-cli"
+PARANAME=para
+SRC_CLI=github.com/33cn/plugin/cli
+
+go build -v -o "${PARACLI}" -ldflags "-X ${SRC_CLI}/buildflags.ParaName=user.p.${PARANAME}. -X ${SRC_CLI}/buildflags.RPCAddr=http://localhost:8901" "${SRC_CLI}"
+# shellcheck disable=SC2086
+cp ./build/* "${OUT_DIR}"
+
+mkdir -p "${OUT_TESTDIR}"
+cp ./build/* "${OUT_TESTDIR}"
