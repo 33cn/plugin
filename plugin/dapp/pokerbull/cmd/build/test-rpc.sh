5d0cd43735eba11edc73e575a13c2b756c99e622
zzh33cn <zzh@33.cn>
2019-06-04 10:47:46 +0800

    update

--- plugin/dapp/pokerbull/cmd/build/test-rpc.sh
+++ plugin/dapp/pokerbull/cmd/build/test-rpc.sh
@@ -78,7 +78,7 @@ pokerbull_QueryResult() {
     echo "========== # pokerbull query result begin =========="
     local req='"method":"Chain33.Query","params":[{"execer":"pokerbull","funcName":"QueryGameByID","payload":{"gameId":"'$GAME_ID'"}}]'
     data=$(curl -ksd "{$req}" ${MAIN_HTTP} | jq -r ".result")
-    ok=$(jq '(.game.gameId == "'$GAME_ID'")' <<<"$data")
+    ok=$(jq '(.game.gameId == "'"$GAME_ID"'")' <<<"$data")
 
     [ "$ok" == true ]
     echo_rst "$FUNCNAME" "$?"
