20a321021f112ec975e4673f4d636896e11d600d
vipwzw <vipwzw@vip.qq.com>
2019-06-04 21:01:20 +0800

    auto ci

--- scp.sh
+++ scp.sh
@@ -2,7 +2,6 @@
 
 hosts=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17)
 
-for x in ${hosts[@]};
-do
-        scp -i ./ycc.test.pem chain33 root@ycc$x:/data/
+for x in ${hosts[@]}; do
+    scp -i ./ycc.test.pem chain33 root@ycc$x:/data/
 done
