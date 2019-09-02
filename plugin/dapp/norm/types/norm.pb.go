c7e3d7e88399f615f937d9cf58599c24ee32cc5b
vipwzw <vipwzw@vip.qq.com>
2019-09-02 15:04:38 +0800

    auto ci

--- plugin/dapp/norm/types/norm.pb.go
+++ plugin/dapp/norm/types/norm.pb.go
@@ -3,9 +3,13 @@
 
 package types
 
-import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
+import (
+	fmt "fmt"
+
+	proto "github.com/golang/protobuf/proto"
+
+	math "math"
+)
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -32,7 +36,7 @@ func (m *Norm) Reset()         { *m = Norm{} }
 func (m *Norm) String() string { return proto.CompactTextString(m) }
 func (*Norm) ProtoMessage()    {}
 func (*Norm) Descriptor() ([]byte, []int) {
-	return fileDescriptor_norm_5c11b861b8e3252c, []int{0}
+	return fileDescriptor_norm_f2aed6c5a9608057, []int{0}
 }
 func (m *Norm) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Norm.Unmarshal(m, b)
@@ -94,7 +98,7 @@ func (m *NormAction) Reset()         { *m = NormAction{} }
 func (m *NormAction) String() string { return proto.CompactTextString(m) }
 func (*NormAction) ProtoMessage()    {}
 func (*NormAction) Descriptor() ([]byte, []int) {
-	return fileDescriptor_norm_5c11b861b8e3252c, []int{1}
+	return fileDescriptor_norm_f2aed6c5a9608057, []int{1}
 }
 func (m *NormAction) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_NormAction.Unmarshal(m, b)
@@ -212,7 +216,7 @@ func (m *NormPut) Reset()         { *m = NormPut{} }
 func (m *NormPut) String() string { return proto.CompactTextString(m) }
 func (*NormPut) ProtoMessage()    {}
 func (*NormPut) Descriptor() ([]byte, []int) {
-	return fileDescriptor_norm_5c11b861b8e3252c, []int{2}
+	return fileDescriptor_norm_f2aed6c5a9608057, []int{2}
 }
 func (m *NormPut) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_NormPut.Unmarshal(m, b)
@@ -257,7 +261,7 @@ func (m *NormGetKey) Reset()         { *m = NormGetKey{} }
 func (m *NormGetKey) String() string { return proto.CompactTextString(m) }
 func (*NormGetKey) ProtoMessage()    {}
 func (*NormGetKey) Descriptor() ([]byte, []int) {
-	return fileDescriptor_norm_5c11b861b8e3252c, []int{3}
+	return fileDescriptor_norm_f2aed6c5a9608057, []int{3}
 }
 func (m *NormGetKey) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_NormGetKey.Unmarshal(m, b)
@@ -291,9 +295,9 @@ func init() {
 	proto.RegisterType((*NormGetKey)(nil), "types.NormGetKey")
 }
 
-func init() { proto.RegisterFile("norm.proto", fileDescriptor_norm_5c11b861b8e3252c) }
+func init() { proto.RegisterFile("norm.proto", fileDescriptor_norm_f2aed6c5a9608057) }
 
-var fileDescriptor_norm_5c11b861b8e3252c = []byte{
+var fileDescriptor_norm_f2aed6c5a9608057 = []byte{
 	// 209 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xca, 0xcb, 0x2f, 0xca,
 	0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2d, 0xa9, 0x2c, 0x48, 0x2d, 0x56, 0x4a, 0xe3,
