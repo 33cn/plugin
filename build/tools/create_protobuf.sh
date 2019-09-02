49efa5601942a1b1477bc4a3761bafb8d0813fa8
linj-disanbo <linj@disanbo.com>
2019-09-02 10:21:52 +0800

    add create_protobuf to build/tools

---
+++ build/tools/create_protobuf.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#set -x
+
+cd $1
+chain33_path=$(go list -f '{{.Dir}}' "github.com/33cn/chain33")
+protoc --go_out=plugins=grpc:../types ./*.proto --proto_path=. --proto_path="${chain33_path}/types/proto/"
+cd -
