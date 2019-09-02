5029ee09a4af6c201c6510ca852591a19ecca164
linj-disanbo <linj@disanbo.com>
2019-09-02 15:00:18 +0800

    fix create_protobuf.sh

--- plugin/dapp/autonomy/proto/create_protobuf.sh
+++ plugin/dapp/autonomy/proto/create_protobuf.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-protoc --go_out=plugins=grpc:../types ./*.proto --proto_path=. --proto_path="$GOPATH/src/github.com/33cn/chain33/types/proto/"
+
+chain33_path=$(go list -f '{{.Dir}}' "github.com/33cn/chain33")
+protoc --go_out=plugins=grpc:../types ./*.proto --proto_path=. --proto_path="${chain33_path}/types/proto/"
