5cb3a78cd58ecffba48d1dae619e72e7e379cc86
zzh33cn <zzh@33.cn>
2019-09-04 10:40:35 +0800

    update

--- plugin/dapp/dposvote/cmd/build/test-rpc.sh
+++ plugin/dapp/dposvote/cmd/build/test-rpc.sh
@@ -19,15 +19,13 @@ RED='\033[1;31m'
 GRE='\033[1;32m'
 NOC='\033[0m'
 
-
 init() {
     ispara=$(echo '"'"${MAIN_HTTP}"'"' | jq '.|contains("8901")')
     echo "ipara=$ispara"
 }
 
 function run_test() {
-
-}
+    echo "run_test"
 }
 
 function main() {
