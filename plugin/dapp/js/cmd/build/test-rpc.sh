d79f790c2b22871221ad39177bc8e3c532f239db
linj-disanbo <linj@disanbo.com>
2019-09-18 14:52:54 +0800

    update address for js

--- plugin/dapp/js/cmd/build/test-rpc.sh
+++ plugin/dapp/js/cmd/build/test-rpc.sh
@@ -9,8 +9,12 @@ MAIN_HTTP=""
 function init() {
     ispara=$(echo '"'"${MAIN_HTTP}"'"' | jq '.|contains("8901")')
 
-    beneficiary=12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv
-    beneficiary_key=0x4257d8692ef7fe13c68b65d6a52f03933db2fa5ce8faf210b5b8b80c721ced01
+    beneficiary_key=0xf146df80206194c81e0b3171db6aa40c7ad6182a24560698d4871d4dc75223ce
+    beneficiary=1DwHQp8S7RS9krQTyrqePxRyvaLcuoQGks
+    chain33_applyCoins "${beneficiary}" 10000000000 "${MAIN_HTTP}"
+    #paracross_get_money 12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv 
+    #//beneficiary=12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv
+    #beneficiary_key=0x4257d8692ef7fe13c68b65d6a52f03933db2fa5ce8faf210b5b8b80c721ced01
     #owner=14KEKbYtKKQm4wMthSK9J4La4nAiidGozt
     #owner_key=CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944
     echo "ipara=$ispara"
@@ -24,6 +28,9 @@ function init() {
         manager_name="user.p.para."${manager_name}
         user_game="user.p.para."${user_game}
         super_manager=0xc34b5d9d44ac7b754806f761d3d4d2c4fe5214f6b074c19f069c4f5c2a29c8cc
+        ## fee
+        local main_ip=${MAIN_HTTP//8901/8801}
+        chain33_applyCoins "${beneficiary}" 10000000000 "${main_ip}"
     fi
     exec_addr=$(curl -ksd '{"method":"Chain33.ConvertExectoAddr","params":[{"execname":"'${exec_name}'"}]}' ${MAIN_HTTP} | jq -r ".result")
     echo "exec_addr=${exec_addr}"
