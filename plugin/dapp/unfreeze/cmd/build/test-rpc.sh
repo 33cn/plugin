d2d61dc23b6f1c48c550ddbe5fa19bacf3080c62
linj-disanbo <linj@disanbo.com>
2019-09-18 17:52:23 +0800

    new account for unfreeze rpc test

--- plugin/dapp/unfreeze/cmd/build/test-rpc.sh
+++ plugin/dapp/unfreeze/cmd/build/test-rpc.sh
@@ -42,25 +42,39 @@ function init() {
     echo "ipara=$ispara"
     exec_name="unfreeze"
     uid_index=2
+    symbol="bty"
+    
+    beneficiary_key=0xa2ec1c6274723c021daa8792f4d0d52ffa0eff0fd47c9c6c1d1dd618762dc178
+    beneficiary=1qpAv7H4C5JBgVQffDRbQKti7ibdM2TfU
+    
+    owner=1CK51xZ1wNkrzAhGyDuFayxeQXHg3gqcVS
+    owner_key=0x3b0d7f65b35da1c394891ba7a8ce0f070ccef6818e3f7ca9c203776013b3a4b0
+
+    chain33_ImportPrivkey "${beneficiary_key}" "${beneficiary}" "unfreeze_beneficiary" "${MAIN_HTTP}"
+    chain33_ImportPrivkey "${owner_key}" "${owner}" "unfreeze_owner" "${MAIN_HTTP}"
+
+    chain33_applyCoins "${beneficiary}" 10000000000 "${MAIN_HTTP}"
+    chain33_applyCoins "${owner}" 10000000000 "${MAIN_HTTP}"
+
     if [ "$ispara" == true ]; then
         exec_name="user.p.para."${exec_name}
         uid_index=1
+        symbol="para"
+
+        local main_ip=${MAIN_HTTP//8901/8801}
+        chain33_applyCoins "${beneficiary}" 10000000000 "${main_ip}"
+        chain33_applyCoins "${owner}" 10000000000 "${main_ip}"
     fi
+    
     exec_addr=$(curl -ksd '{"method":"Chain33.ConvertExectoAddr","params":[{"execname":"'${exec_name}'"}]}' ${MAIN_HTTP} | jq -r ".result")
     echo "exec_addr=${exec_addr}"
-
-    beneficiary=1PUiGcbsccfxW3zuvHXZBJfznziph5miAo
-    beneficiary_key=0x56942AD84CCF4788ED6DACBC005A1D0C4F91B63BCF0C99A02BE03C8DEAE71138
-    owner=1EDnnePAZN48aC2hiTDzhkczfF39g1pZZX
-    owner_key=0x2116459C0EC8ED01AA0EEAE35CAC5C96F94473F7816F114873291217303F6989
-
-    chain33_SendToAddress "$owner" "$exec_addr" 500000000 "${MAIN_HTTP}"
-    chain33_SendToAddress "$beneficiary" "$exec_addr" 500000000 "${MAIN_HTTP}"
+    chain33_SendToAddress "$owner" "$exec_addr" 5000000000 "${MAIN_HTTP}"
+    chain33_SendToAddress "$beneficiary" "$exec_addr" 5000000000 "${MAIN_HTTP}"
     chain33_BlockWait 1 "${MAIN_HTTP}"
 }
 
 function CreateRawUnfreezeCreate() {
-    req='{"jsonrpc": "2.0", "method" :  "unfreeze.CreateRawUnfreezeCreate" , "params":[{"startTime":10000,"assetExec":"coins","assetSymbol":"bty","totalCount":400000000,"beneficiary":"'$beneficiary'","means":"FixAmount","fixAmount": {"period":10,"amount":1000000}}]}'
+    req='{"jsonrpc": "2.0", "method" :  "unfreeze.CreateRawUnfreezeCreate" , "params":[{"startTime":10000,"assetExec":"coins","assetSymbol":"'$symbol'","totalCount":400000000,"beneficiary":"'$beneficiary'","means":"FixAmount","fixAmount": {"period":10,"amount":1000000}}]}'
     # echo "#request: $req"
     resp=$(curl -ksd "$req" "${MAIN_HTTP}")
     # echo "#resp: $resp"
