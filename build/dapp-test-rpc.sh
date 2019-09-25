d6c7be312ea8da096835e02f66ab61a174c0373d
mdj33 <travis@travis-ci.org>
2019-09-25 17:12:27 +0800

    ticket add to parallel

--- build/dapp-test-rpc.sh
+++ build/dapp-test-rpc.sh
@@ -11,14 +11,14 @@ function dapp_test_rpc() {
         cp "$DAPP_TEST_COMMON" dapptest/
         cd dapptest || return
 
-        dapps=$(find . -maxdepth 1 -type d ! -name dapptest ! -name ticket ! -name . | sed 's/^\.\///' | sort)
+        dapps=$(find . -maxdepth 1 -type d ! -name dapptest  ! -name . | sed 's/^\.\///' | sort)
         echo "dapps list: $dapps"
-        parallel -k --retries 3 --verbose --joblog ./testlog ./{}/"${RPC_TESTFILE}" "$ip" ::: "$dapps"
+        parallel -k --retries 3 --joblog ./testlog ./{}/"${RPC_TESTFILE}" "$ip" ::: "$dapps"
         echo "check dapps test log"
         cat ./testlog
 
         ##ticket用例最后执行
-        ./ticket/"${RPC_TESTFILE}" "$ip"
+        #./ticket/"${RPC_TESTFILE}" "$ip"
     fi
     echo "============ # dapp rpc test end ============="
 }
