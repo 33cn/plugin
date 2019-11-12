2d38ee7d320b899542ae02d0bda4ba66b1da1bef
mdj33 <travis@travis-ci.org>
2019-11-12 14:24:58 +0800

    code improve

--- plugin/consensus/para/paracommitmsg_test.go
+++ plugin/consensus/para/paracommitmsg_test.go
@@ -7,7 +7,11 @@ package para
 import (
 	"testing"
 
+	"github.com/33cn/chain33/queue"
 	_ "github.com/33cn/chain33/system"
+	drivers "github.com/33cn/chain33/system/consensus"
+	"github.com/33cn/chain33/types"
+	"github.com/33cn/plugin/plugin/dapp/paracross/testnode"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,4 +32,42 @@ func TestIsSelfConsEnable(t *testing.T) {
 	assert.Equal(t, false, enable)
 	enable = commitCli.isSelfConsEnable(30)
 	assert.Equal(t, true, enable)
+}
+
+func TestParseSelfConsEnableStr(t *testing.T) {
+	t1 := []string{"1-100", "200-300"}
+	e1 := &paraSelfConsEnable{startHeight: 1, endHeight: 100}
+	e2 := &paraSelfConsEnable{startHeight: 200, endHeight: 300}
+	ep1 := []*paraSelfConsEnable{e1, e2}
+	t2 := []string{"1-100", "200-"}
+
+	l1, err := parseSelfConsEnableStr(t1)
+	assert.Nil(t, err)
+	assert.Equal(t, ep1, l1)
+
+	l2, err := parseSelfConsEnableStr(t2)
+	assert.NotNil(t, err)
+	assert.Nil(t, l2)
+}
+
+func TestSetSelfConsEnable(t *testing.T) {
+	cfg := types.NewChain33Config(testnode.DefaultConfig)
+	q := queue.New("channel")
+	q.SetConfig(cfg)
+	para := new(client)
+	para.subCfg = new(subConfig)
+
+	baseCli := drivers.NewBaseClient(&types.Consensus{Name: "name"})
+	para.BaseClient = baseCli
+
+	para.InitClient(q.Client(), initTestSyncBlock)
+	para.commitMsgClient = &commitMsgClient{
+		paraClient: para,
+	}
+	err := para.commitMsgClient.setSelfConsEnable()
+	assert.Nil(t, err)
+	e1 := &paraSelfConsEnable{startHeight: 0, endHeight: 1000}
+	ep1 := []*paraSelfConsEnable{e1}
+	assert.Equal(t, ep1, para.commitMsgClient.selfConsEnableList)
+
 }
