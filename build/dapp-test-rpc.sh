497d30b07e2eaeda9d1a7bd18682e25b52f92eaf
mdj33 <travis@travis-ci.org>
2019-11-22 14:17:15 +0800

    fix linter

--- build/dapp-test-rpc.sh
+++ build/dapp-test-rpc.sh
@@ -11,6 +11,7 @@ function dapp_test_rpc() {
         cp "$DAPP_TEST_COMMON" dapptest/
         cd dapptest || return
         rm -f "jobs.log"
+        rm -rf "outdir"
 
         dapps=$(find . -maxdepth 1 -type d ! -name dapptest ! -name . | sed 's/^\.\///' | sort)
         echo "dapps list: $dapps"
@@ -18,7 +19,7 @@ function dapp_test_rpc() {
         parallel -k --results outdir --joblog ./jobs.log ./{}/"${RPC_TESTFILE}" "$ip" ::: "$dapps"
         local ret=$?
         if [ $ret -ne 0 ]; then
-            wrongdapps=$(cat jobs.log | awk '{print $7,$9 }' | grep -a 1 | awk -F '/' '{print $2}')
+            wrongdapps=$(awk '{print $7,$9 }' jobs.log | grep -a 1 | awk -F '/' '{print $2}')
             parallel -k 'cat ./outdir/1/{}/stderr; cat ./outdir/1/{}/stdout' ::: "$wrongdapps"
         fi
         echo "============ # check dapps test log: ============="
