244977d750c3fbceec2ce6febed65ad4f06922bc
harrylee <18761806026@163.com>
2019-11-27 18:12:49 +0800

    add storage dapp

---
+++ plugin/dapp/storage/executor/kv.go
@@ -0,0 +1,20 @@
+package executor
+
+/*
+ * 用户合约存取kv数据时，key值前缀需要满足一定规范
+ * 即key = keyPrefix + userKey
+ * 需要字段前缀查询时，使用’-‘作为分割符号
+ */
+
+var (
+	//KeyPrefixStateDB state db key必须前缀
+	KeyPrefixStateDB = "mavl-storage-"
+	//KeyPrefixLocalDB local db的key必须前缀
+	KeyPrefixLocalDB = "LODB-storage-"
+)
+// Key Storage to save key
+func Key(txHash string) (key []byte) {
+	key = append(key, []byte(KeyPrefixStateDB)...)
+	key = append(key, []byte(txHash)...)
+	return key
+}
