139dae83db0f14a7157f0de7872a95dd3797e8dd
pengjun <pengj@disanbo.com>
2019-12-05 10:39:02 +0800

    #627 add rpc-test

---
+++ plugin/dapp/collateralize/cmd/test/test-rpc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2128
+set -e
+set -o pipefail
+
+
+function main() {
+  echo "Collateralize cases has integrated in Issuance test"
+}
+
+chain33_debug_function main "$1"
--- plugin/dapp/issuance/cmd/build.sh
+++ plugin/dapp/issuance/cmd/build.sh
@@ -1,15 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 strpwd=$(pwd)
 strcmd=${strpwd##*dapp/}
 strapp=${strcmd%/cmd*}
 
-OUT_DIR="${1}/$strapp"
-#FLAG=$2
-
-mkdir -p "${OUT_DIR}"
-cp ./build/* "${OUT_DIR}"
-
 OUT_TESTDIR="${1}/dapptest/$strapp"
 mkdir -p "${OUT_TESTDIR}"
-cp ./build/test-rpc.sh "${OUT_TESTDIR}"
+cp ./test/test-rpc.sh "${OUT_TESTDIR}"
