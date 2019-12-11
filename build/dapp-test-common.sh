1448e30d5ce8466ac8c2fde75254b0f546550892
yingqiumin@126.com <yingqiumin@126.com>
2019-12-11 14:47:51 +0800

    improve rpc test

--- build/dapp-test-common.sh
+++ build/dapp-test-common.sh
@@ -38,7 +38,7 @@ chain33_Http() {
 chain33_DecodeRawTransactionTx() {
     # txHex="$1" priKey="$2" MAIN_HTTP="$3" FUNCNAME="$4"
     req='{"method":"Chain33.DecodeRawTransaction","params":[{"txHex":"'"$1"'"}]}'
-    chain33_Http "$req" "$3" '(.result.txs[0].execer != null)' "$4"
+    chain33_Http "$req" "$3" '(.result.txs[0].execer != "") and (.result.txs[0].execer != null)' "$4"
     chain33_SignRawTx "$1" "$2" "$3"
     chain33_BlockWait 1 "$3"
 }
