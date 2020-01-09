252cb971d8e8af2f8b2fc8a6cb5bf78f6a6265ea
linj-disanbo <linj@disanbo.com>
2020-01-09 11:19:42 +0800

    移动代码

--- plugin/dapp/trade/executor/kv.go
+++ plugin/dapp/trade/executor/kv.go
@@ -4,27 +4,9 @@
 
 package executor
 
-import (
-	"fmt"
-)
-
 const (
 	sellIDPrefix = "mavl-trade-sell-"
 	buyIDPrefix  = "mavl-trade-buy-"
-)
-
-// 下个版本可以删除
-const (
-	sellOrderSHTAS = "LODB-trade-sellorder-shtas:"
-	sellOrderASTS  = "LODB-trade-sellorder-asts:"
-	sellOrderATSS  = "LODB-trade-sellorder-atss:"
-	sellOrderTSPAS = "LODB-trade-sellorder-tspas:"
-	buyOrderSHTAS  = "LODB-trade-buyorder-shtas:"
-	buyOrderASTS   = "LODB-trade-buyorder-asts:"
-	buyOrderATSS   = "LODB-trade-buyorder-atss:"
-	buyOrderTSPAS  = "LODB-trade-buyorder-tspas:"
-	// Addr-Status-Type-Height-Key
-	orderASTHK = "LODB-trade-order-asthk:"
 )
 
 // ids
@@ -36,34 +18,9 @@ func calcTokenBuyID(hash string) string {
 	return buyIDPrefix + hash
 }
 
-// 特定帐号下的订单
-// 这里状态进行转化, 分成 状态和类型， 状态三种， 类型 两种
-//  on:  OnSale OnBuy
-//  done:  Soldout boughtOut
-//  revoke:  RevokeSell RevokeBuy
-// buy/sell 两种类型
-//  目前页面是按addr， 状态来
-
 // make a number as token's price whether cheap or dear
 // support 1e8 bty pre token or 1/1e8 bty pre token, [1Coins, 1e16Coins]
 // the number in key is used to sort buy orders and pages
 func calcPriceOfToken(priceBoardlot, AmountPerBoardlot int64) int64 {
 	return 1e8 * priceBoardlot / AmountPerBoardlot
-}
-
-// UpdateLocalDBPart1 手动生成KV，需要在原有数据库中删除
-// TODO
-func UpdateLocalDBPart1() {
-	prefix := []string{
-		sellOrderSHTAS,
-		sellOrderASTS,
-		sellOrderATSS,
-		sellOrderTSPAS,
-		buyOrderSHTAS,
-		buyOrderASTS,
-		buyOrderATSS,
-		buyOrderTSPAS,
-		orderASTHK,
-	}
-	fmt.Printf("%+v", prefix)
 }
--- plugin/dapp/trade/executor/localdb_v1.go
+++ plugin/dapp/trade/executor/localdb_v1.go
@@ -1,8 +1,41 @@
 package executor
 
+import "fmt"
+
 // 生成 key -> id 格式的本地数据库数据， 在下个版本这个文件可以全部删除
 // 由于数据库精简需要保存具体数据
 
 // 将手动生成的local db 的代码和用table 生成的local db的代码分离出来
 // 手动生成的local db, 将不生成任意资产标价的数据， 保留用coins 生成交易的数据， 来兼容为升级的app 应用
 // 希望有全量数据的， 需要调用新的rpc
+
+// 下个版本可以删除
+const (
+	sellOrderSHTAS = "LODB-trade-sellorder-shtas:"
+	sellOrderASTS  = "LODB-trade-sellorder-asts:"
+	sellOrderATSS  = "LODB-trade-sellorder-atss:"
+	sellOrderTSPAS = "LODB-trade-sellorder-tspas:"
+	buyOrderSHTAS  = "LODB-trade-buyorder-shtas:"
+	buyOrderASTS   = "LODB-trade-buyorder-asts:"
+	buyOrderATSS   = "LODB-trade-buyorder-atss:"
+	buyOrderTSPAS  = "LODB-trade-buyorder-tspas:"
+	// Addr-Status-Type-Height-Key
+	orderASTHK = "LODB-trade-order-asthk:"
+)
+
+// UpdateLocalDBPart1 手动生成KV，需要在原有数据库中删除
+// TODO
+func UpdateLocalDBPart1() {
+	prefix := []string{
+		sellOrderSHTAS,
+		sellOrderASTS,
+		sellOrderATSS,
+		sellOrderTSPAS,
+		buyOrderSHTAS,
+		buyOrderASTS,
+		buyOrderATSS,
+		buyOrderTSPAS,
+		orderASTHK,
+	}
+	fmt.Printf("%+v", prefix)
+}
