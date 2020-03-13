9b69fd1031fbf806a6acc20858ee395a51dcbe56
jiangpeng <jiangp@33.cn>
2020-03-13 14:28:06 +0800

    plugin:import p2p plugin gossip

---
+++ plugin/p2p/init/init.go
@@ -0,0 +1,10 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package init init p2p
+package init
+
+import (
+	_ "github.com/33cn/plugin/plugin/p2p/gossip" //原生gossip p2p插件
+)
