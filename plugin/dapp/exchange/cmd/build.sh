7a46448694bef9f9ce682930939c75a5173c8eba
harrylee <18761806026@163.com>
2020-06-23 17:05:15 +0800

    comment out unwanted code for shell check

--- plugin/dapp/exchange/cmd/build.sh
+++ plugin/dapp/exchange/cmd/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # 官方ci集成脚本
-strpwd=$(pwd)
-strcmd=${strpwd##*dapp/}
-strapp=${strcmd%/cmd*}
-
-OUT_DIR="${1}/$strapp"
+#strpwd=$(pwd)
+#strcmd=${strpwd##*dapp/}
+#strapp=${strcmd%/cmd*}
+#
+#OUT_DIR="${1}/$strapp"
 #FLAG=$2
 
 # mkdir -p "${OUT_DIR}"
