97383193757a24d688ca7bb4e5f6472dcce53f9c
mdj33 <mdj@disanbo.com>
2020-07-30 16:12:54 +0800

    comment x2eth

--- build/docker-compose-pre.sh
+++ build/docker-compose-pre.sh
@@ -101,7 +101,7 @@ function main() {
             echo "============ run main end ================="
 
             find . -maxdepth 1 -type d -name "*-ci" -exec rm -rf {} \;
-            dir=$(find . -maxdepth 1 -type d ! -name system ! -name . | sed 's/^\.\///')
+            dir=$(find . -maxdepth 1 -type d ! -name system ! -name x2ethereum ! -name . | sed 's/^\.\///')
             for app in $dir; do
                 run_single_app "${app}" "$TESTCASEFILE" "down"
             done
