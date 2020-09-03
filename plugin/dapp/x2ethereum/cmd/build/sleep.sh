3149bc220155afc3a064cf48270fa23ddccd2b65
QM <yingqiumin@126.com>
2020-09-03 17:29:38 +0800

    updata

--- plugin/dapp/x2ethereum/cmd/build/sleep.sh
+++ plugin/dapp/x2ethereum/cmd/build/sleep.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2128
+# shellcheck disable=SC2050
 # shellcheck source=/dev/null
 set -x
 set +e
 
 while [ 1 == 1 ]; do
-    sleep 100
+     sleep 100
 done
