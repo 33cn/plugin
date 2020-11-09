9b17cec36e0d1dbfafc2728a3c03c89211c9069c
mdj33 <mdj@disanbo.com>
2020-11-09 19:34:26 +0800

    decimal config

--- plugin/dapp/paracross/executor/action.go
+++ plugin/dapp/paracross/executor/action.go
@@ -1300,12 +1300,17 @@ func (a *action) Miner(miner *pt.ParacrossMinerAction) (*types.Receipt, error) {
 		}
 		totalReward *= types.Coin
 
+		decimalMode := cfg.MIsEnable("mver.consensus.paracross.decimalMode", a.height)
+		if !decimalMode {
+			totalReward *= types.Coin
+		}
+
 		if totalReward > 0 {
 			issueReceipt, err := a.coinsAccount.ExecIssueCoins(a.execaddr, totalReward)
 
 			if err != nil {
 				clog.Error("paracross miner issue err", "height", miner.Status.Height,
-					"execAddr", a.execaddr, "amount", totalReward/types.Coin)
+					"execAddr", a.execaddr, "amount", totalReward)
 				return nil, err
 			}
 			minerReceipt = mergeReceipt(minerReceipt, issueReceipt)
