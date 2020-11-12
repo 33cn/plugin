2d9b1a4f02e7e1434d26b896b9ff32100b0484fb
jiangpeng <jiangp@33.cn>
2020-11-12 16:47:40 +0800

    p2p:update gossip broadcast
    * add dial grpc call option, max msg size
    * update light block broadcast config

--- plugin/p2p/gossip/process_test.go
+++ plugin/p2p/gossip/process_test.go
@@ -27,6 +27,7 @@ func Test_processP2P(t *testing.T) {
 	q.SetConfig(cfg)
 	go q.Start()
 	p2p := newP2p(cfg, 12345, "testProcessP2p", q)
+	p2p.subCfg.MinLtBlockSize = 0
 	defer freeP2p(p2p)
 	defer q.Close()
 	node := p2p.node
@@ -34,7 +35,6 @@ func Test_processP2P(t *testing.T) {
 	pid := "testPid"
 	sendChan := make(chan interface{}, 1)
 	recvChan := make(chan *types.BroadCastData, 1)
-	testDone := make(chan struct{})
 
 	payload := []byte("testpayload")
 	minerTx := &types.Transaction{Execer: []byte("coins"), Payload: payload, Fee: 14600, Expire: 200}
@@ -106,73 +106,75 @@ func Test_processP2P(t *testing.T) {
 	}()
 
 	//data test
-	go func() {
-		subChan := node.pubsub.Sub(pid)
-		//normal
-		sendChan <- &versionData{peerName: pid + "1", rawData: &types.P2PTx{Tx: tx, Route: &types.P2PRoute{}}, version: lightBroadCastVersion - 1}
-		p2p.mgr.PubSub.Pub(client.NewMessage("p2p", types.EventTxBroadcast, tx), P2PTypeName)
-		sendChan <- &versionData{peerName: pid + "1", rawData: &types.P2PBlock{Block: block}, version: lightBroadCastVersion - 1}
-		//light broadcast
-		txHashFilter.Add(hex.EncodeToString(tx1.Hash()), &types.P2PRoute{TTL: DefaultLtTxBroadCastTTL})
-		p2p.mgr.PubSub.Pub(client.NewMessage("p2p", types.EventTxBroadcast, tx1), P2PTypeName)
-		sendChan <- &versionData{peerName: pid + "2", rawData: &types.P2PTx{Tx: tx, Route: &types.P2PRoute{TTL: DefaultLtTxBroadCastTTL}}, version: lightBroadCastVersion}
-		<-subChan //query tx
-		sendChan <- &versionData{peerName: pid + "2", rawData: &types.P2PBlock{Block: block}, version: lightBroadCastVersion}
-		<-subChan //query block
-		for !ltBlockCache.Contains(blockHash) {
-		}
+	subChan := node.pubsub.Sub(pid)
+	//全数据广播
+	sendChan <- &versionData{peerName: pid + "1", rawData: &types.P2PTx{Tx: tx, Route: &types.P2PRoute{}}, version: lightBroadCastVersion - 1}
+	p2p.mgr.PubSub.Pub(client.NewMessage("p2p", types.EventTxBroadcast, tx), P2PTypeName)
+	sendChan <- &versionData{peerName: pid + "1", rawData: &types.P2PBlock{Block: block}, version: lightBroadCastVersion - 1}
+	//交易发送过滤
+	txHashFilter.Add(hex.EncodeToString(tx1.Hash()), &types.P2PRoute{TTL: DefaultLtTxBroadCastTTL})
+	p2p.mgr.PubSub.Pub(client.NewMessage("p2p", types.EventTxBroadcast, tx1), P2PTypeName)
+	//交易短哈希广播
+	sendChan <- &versionData{peerName: pid + "2", rawData: &types.P2PTx{Tx: tx, Route: &types.P2PRoute{TTL: DefaultLtTxBroadCastTTL}}, version: lightBroadCastVersion}
+	recvWithTimeout(t, subChan, "case 1") //缺失交易，从对端获取
+	//区块短哈希广播
+	sendChan <- &versionData{peerName: pid + "2", rawData: &types.P2PBlock{Block: block}, version: lightBroadCastVersion}
+	recvWithTimeout(t, subChan, "case 2")
+	assert.True(t, ltBlockCache.Contains(blockHash))
+	cpBlock := *ltBlockCache.Get(blockHash).(*types.Block)
+	assert.True(t, bytes.Equal(rootHash, merkle.CalcMerkleRoot(cfg, cpBlock.Height, cpBlock.Txs)))
 
-		cpBlock := *ltBlockCache.Get(blockHash).(*types.Block)
-		assert.True(t, bytes.Equal(rootHash, merkle.CalcMerkleRoot(cfg, cpBlock.Height, cpBlock.Txs)))
+	//query tx
+	sendChan <- &versionData{rawData: &types.P2PQueryData{Value: &types.P2PQueryData_TxReq{TxReq: &types.P2PTxReq{TxHash: tx.Hash()}}}}
+	data := recvWithTimeout(t, subChan, "case 3")
+	_, ok := data.(*types.P2PTx)
+	assert.True(t, ok)
+	sendChan <- &versionData{rawData: &types.P2PQueryData{Value: &types.P2PQueryData_BlockTxReq{BlockTxReq: &types.P2PBlockTxReq{
+		BlockHash: blockHash,
+		TxIndices: []int32{1, 2},
+	}}}}
+	data = recvWithTimeout(t, subChan, "case 4")
+	rep, ok := data.(*types.P2PBlockTxReply)
+	assert.True(t, ok)
+	assert.Equal(t, 2, int(rep.TxIndices[1]))
+	sendChan <- &versionData{rawData: &types.P2PQueryData{Value: &types.P2PQueryData_BlockTxReq{BlockTxReq: &types.P2PBlockTxReq{
+		BlockHash: blockHash,
+		TxIndices: nil,
+	}}}}
+	data = recvWithTimeout(t, subChan, "case 5")
+	rep, ok = data.(*types.P2PBlockTxReply)
+	assert.True(t, ok)
+	assert.Nil(t, rep.TxIndices)
 
-		//query tx
-		sendChan <- &versionData{rawData: &types.P2PQueryData{Value: &types.P2PQueryData_TxReq{TxReq: &types.P2PTxReq{TxHash: tx.Hash()}}}}
-		_, ok := (<-subChan).(*types.P2PTx)
-		assert.True(t, ok)
-		sendChan <- &versionData{rawData: &types.P2PQueryData{Value: &types.P2PQueryData_BlockTxReq{BlockTxReq: &types.P2PBlockTxReq{
-			BlockHash: blockHash,
-			TxIndices: []int32{1, 2},
-		}}}}
-		rep, ok := (<-subChan).(*types.P2PBlockTxReply)
-		assert.True(t, ok)
-		assert.Equal(t, 2, int(rep.TxIndices[1]))
-		sendChan <- &versionData{rawData: &types.P2PQueryData{Value: &types.P2PQueryData_BlockTxReq{BlockTxReq: &types.P2PBlockTxReq{
-			BlockHash: blockHash,
-			TxIndices: nil,
-		}}}}
-		rep, ok = (<-subChan).(*types.P2PBlockTxReply)
-		assert.True(t, ok)
-		assert.Nil(t, rep.TxIndices)
-
-		//query reply
-		sendChan <- &versionData{rawData: &types.P2PBlockTxReply{
-			BlockHash: blockHash,
-			TxIndices: []int32{1},
-			Txs:       txList[1:2],
-		}}
-		rep1, ok := (<-subChan).(*types.P2PQueryData)
-		assert.True(t, ok)
-		assert.Nil(t, rep1.GetBlockTxReq().GetTxIndices())
-		sendChan <- &versionData{rawData: &types.P2PBlockTxReply{
-			BlockHash: blockHash,
-			Txs:       txList[0:],
-		}}
-		for ltBlockCache.Contains(blockHash) {
-		}
-		//max ttl
-		_, doSend := node.processSendP2P(&types.P2PTx{Tx: tx, Route: &types.P2PRoute{TTL: node.nodeInfo.cfg.MaxTTL + 1}}, lightBroadCastVersion, pid+"5", "testIP:port")
-		assert.False(t, doSend)
-		close(testDone)
-	}()
-	ticker := time.NewTicker(time.Minute)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-testDone:
-			return
-		case <-ticker.C:
-			t.Error("TestP2PProcessTimeout")
-			return
-		}
+	//query reply
+	sendChan <- &versionData{rawData: &types.P2PBlockTxReply{
+		BlockHash: blockHash,
+		TxIndices: []int32{1},
+		Txs:       txList[1:2],
+	}}
+	rep1, ok := recvWithTimeout(t, subChan, "case 6").(*types.P2PQueryData)
+	assert.True(t, ok)
+	assert.Nil(t, rep1.GetBlockTxReq().GetTxIndices())
+	sendChan <- &versionData{rawData: &types.P2PBlockTxReply{
+		BlockHash: blockHash,
+		Txs:       txList[0:],
+	}}
+	for ltBlockCache.Contains(blockHash) {
+		time.Sleep(time.Millisecond)
 	}
+	//send tx with max ttl
+	_, doSend := node.processSendP2P(&types.P2PTx{Tx: tx, Route: &types.P2PRoute{TTL: node.nodeInfo.cfg.MaxTTL + 1}}, lightBroadCastVersion, pid+"5", "testIP:port")
+	assert.False(t, doSend)
+}
+
+// 等待接收channel数据，超时报错
+func recvWithTimeout(t *testing.T, ch chan interface{}, testCase string) interface{} {
+	select {
+	case data := <-ch:
+		return data
+	case <-time.After(time.Second * 10):
+		t.Error(testCase, "waitChanTimeout")
+		t.FailNow()
+	}
+	return nil
 }
