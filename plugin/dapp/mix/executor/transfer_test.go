13105aded5b3e1d56ce1b86b35c050bbe2bc7d79
mdj33 <mdj@disanbo.com>
2020-11-16 19:33:36 +0800

    add circuit test

--- plugin/dapp/mix/executor/transfer_test.go
+++ plugin/dapp/mix/executor/transfer_test.go
@@ -37,3 +37,30 @@ func TestVerifyCommitValues(t *testing.T) {
 	assert.Equal(t, true, ret)
 
 }
+
+func TestVerifyCommitValues2(t *testing.T) {
+	input1 := &mixTy.TransferInputPublicInput{
+		CommitValueX: "10190477835300927557649934238820360529458681672073866116232821892325659279502",
+		CommitValueY: "7969140283216448215269095418467361784159407896899334866715345504515077887397",
+	}
+
+	input2 := &mixTy.TransferInputPublicInput{
+		CommitValueX: "17822967620457187568904804290291537271142779717280482398091401115827760898835",
+		CommitValueY: "17714526567340249480661526843742175665966437069228179299143955140199226385576",
+	}
+
+	var inputs []*mixTy.TransferInputPublicInput
+	inputs = append(inputs, input1)
+	inputs = append(inputs, input2)
+
+	output1 := &mixTy.TransferOutputPublicInput{
+		CommitValueX: "14087975867275911077371231345227824611951436822132762463787130558957838320348",
+		CommitValueY: "15113519960384204624879642069520481336224311978035289236693658603675385299879",
+	}
+	var outputs []*mixTy.TransferOutputPublicInput
+	outputs = append(outputs, output1)
+
+	ret := verifyCommitValues(inputs, outputs)
+	assert.Equal(t, true, ret)
+
+}
