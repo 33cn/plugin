5eea0c0c689ee6074f2917bd8813ce74e42d203d
QM <yingqiumin@126.com>
2020-11-25 15:51:48 +0800

    add supervision node modify unit test

--- plugin/dapp/paracross/executor/supervisionaccount_test.go
+++ plugin/dapp/paracross/executor/supervisionaccount_test.go
@@ -27,6 +27,7 @@ func (suite *NodeManageTestSuite) testSupervisionExec() {
 	suite.testSupervisionNodeError()
 	suite.testSupervisionQuery()
 	suite.testSupervisionNodeQuit()
+	suite.testSupervisionNodeModify()
 }
 
 func (suite *NodeManageTestSuite) testSupervisionNodeConfigCancel(addr, privKey string) {
@@ -88,7 +89,7 @@ func (suite *NodeManageTestSuite) testSupervisionNodeError() {
 	tx := createRawSupervisionNodeConfigTx(config)
 	tx, _ = signTx(suite.Suite, tx, PrivKey1M3)
 	_, err := suite.exec.Exec(tx, 0)
-	assert.Equal(suite.T(), err, pt.ErrParaSupervisionNodeAddrExisted)
+	suite.NotNil(err)
 
 	config = &pt.ParaNodeAddrConfig{
 		Title: chain33TestCfg.GetTitle(),
@@ -98,7 +99,7 @@ func (suite *NodeManageTestSuite) testSupervisionNodeError() {
 	tx = createRawSupervisionNodeConfigTx(config)
 	tx, _ = signTx(suite.Suite, tx, PrivKey1KS)
 	_, err = suite.exec.Exec(tx, 0)
-	assert.Equal(suite.T(), err, pt.ErrParaNodeAddrExisted)
+	suite.NotNil(err)
 }
 
 func (suite *NodeManageTestSuite) testSupervisionNodeQuit() {
@@ -119,6 +120,28 @@ func (suite *NodeManageTestSuite) testSupervisionNodeQuit() {
 	resp, ok := ret.(*types.ReplyConfig)
 	assert.Equal(suite.T(), ok, true)
 	assert.Equal(suite.T(), resp.Value, "14KEKbYtKKQm4wMthSK9J4La4nAiidGozt,1M3XCbWVxAPBH5AR8VmLky4ZtDdGgC6ugD")
+}
+
+func (suite *NodeManageTestSuite) testSupervisionNodeModify() {
+	config := &pt.ParaNodeAddrConfig{
+		Title:     chain33TestCfg.GetTitle(),
+		Op:        pt.ParacrossSupervisionNodeModify,
+		Addr:      Account14K,
+		BlsPubKey: Bls14K,
+	}
+	tx := createRawSupervisionNodeConfigTx(config)
+	receipt := nodeCommit(suite, PrivKey14K, tx)
+	assert.Equal(suite.T(), receipt.Ty, int32(types.ExecOk))
+	assert.Len(suite.T(), receipt.KV, 1)
+	assert.Len(suite.T(), receipt.Logs, 1)
+	assert.Equal(suite.T(), int32(pt.TyLogParaSupervisionNodeStatusUpdate), receipt.Logs[0].Ty)
+
+	ret, err := suite.exec.Query_GetNodeAddrInfo(&pt.ReqParacrossNodeInfo{Title: chain33TestCfg.GetTitle(), Addr: Account14K})
+	suite.Nil(err)
+	resp, ok := ret.(*pt.ParaNodeAddrIdStatus)
+	assert.Equal(suite.T(), ok, true)
+	assert.NotNil(suite.T(), resp)
+	assert.Equal(suite.T(), resp.BlsPubKey, Bls14K)
 }
 
 func checkSupervisionGroupApplyReceipt(suite *NodeManageTestSuite, receipt *types.Receipt) {
