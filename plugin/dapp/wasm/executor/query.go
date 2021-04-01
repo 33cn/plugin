261bb43b3411eea957f4909b7308685994cd3832
yxq <yxq@disanbo.com>
2021-04-01 17:21:08 +0800

    add query db

--- plugin/dapp/wasm/executor/query.go
+++ plugin/dapp/wasm/executor/query.go
@@ -9,5 +9,36 @@ func (w *Wasm) Query_Check(query *types2.QueryCheckContract) (types.Message, err
 	if query == nil {
 		return nil, types.ErrInvalidParam
 	}
-	return &types.Reply{IsOk: w.contractExist(query.Name)}, nil
+	_, err := w.GetStateDB().Get(contractKey(query.Name))
+	if err == nil {
+		return &types.Reply{IsOk: true}, nil
+	}
+	if err == types.ErrNotFound {
+		return &types.Reply{}, nil
+	}
+	return nil, err
+}
+
+func (w *Wasm) Query_QueryStateDB(query *types2.QueryContractDB) (types.Message, error) {
+	if query == nil {
+		return nil, types.ErrInvalidParam
+	}
+	key := append(calcStatePrefix(query.Contract), query.Key...)
+	v, err := w.GetStateDB().Get(key)
+	if err != nil {
+		return nil, err
+	}
+	return &types.ReplyString{Data: string(v)}, nil
+}
+
+func (w *Wasm) Query_QueryLocalDB(query *types2.QueryContractDB) (types.Message, error) {
+	if query == nil {
+		return nil, types.ErrInvalidParam
+	}
+	key := append(calcLocalPrefix(query.Contract), query.Key...)
+	v, err := w.GetLocalDB().Get(key)
+	if err != nil {
+		return nil, err
+	}
+	return &types.ReplyString{Data: string(v)}, nil
 }
