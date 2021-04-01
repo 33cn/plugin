8c1192ffdfea67b467bdd4cb4a11f0ebf8b19543
yxq <yxq@disanbo.com>
2021-04-01 09:54:33 +0800

    wasm contract can be updated by the creator

--- plugin/dapp/wasm/executor/exec_del_local.go
+++ plugin/dapp/wasm/executor/exec_del_local.go
@@ -9,6 +9,10 @@ func (w *Wasm) ExecDelLocal_Create(payload *types2.WasmCreate, tx *types.Transac
 	return &types.LocalDBSet{}, nil
 }
 
+func (w *Wasm) ExecDelLocal_Update(payload *types2.WasmUpdate, tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return &types.LocalDBSet{}, nil
+}
+
 func (w *Wasm) ExecDelLocal_Call(payload *types2.WasmCall, tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
 	localExecer := w.userExecName(payload.Contract, true)
 	kvs, err := w.DelRollbackKV(tx, []byte(localExecer))
