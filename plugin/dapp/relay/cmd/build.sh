cfbc420ac3c6e931e22bb569ff93de3abad59a20
mdj33 <mdj33@disanbo.com>
2021-07-21 10:22:13 +0800

    update chain33

--- plugin/dapp/relay/cmd/build.sh
+++ plugin/dapp/relay/cmd/build.sh
@@ -9,7 +9,7 @@ SRC_RELAYD=github.com/33cn/plugin/plugin/dapp/relay/cmd/relayd
 FLAG=$2
 
 # shellcheck disable=SC2086,1072
-go build -i ${FLAG} -v -o "${OUT_DIR}/relayd" "${SRC_RELAYD}"
+go build ${FLAG} -v -o "${OUT_DIR}/relayd" "${SRC_RELAYD}"
 cp ./relayd/relayd.toml "${OUT_DIR}/relayd.toml"
 cp ./build/* "${OUT_DIR}"
 
