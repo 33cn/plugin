5eead494b1b4180766ebd23e7645b69ed8f8799f
zhengjunhe <hzj@disanbo.com>
2021-09-14 17:57:34 +0800

    remove unused test case

--- plugin/dapp/cross2eth/ebrelayer/relayer/events/events_test.go
+++ plugin/dapp/cross2eth/ebrelayer/relayer/events/events_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/33cn/plugin/plugin/dapp/cross2eth/contracts/contracts4eth/generated"
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -22,12 +21,4 @@ func Test_UnpackLogLock(t *testing.T) {
 
 	_, err = UnpackLogBurn(contractABI, eventName, eventData)
 	require.NotNil(t, err)
-}
-
-func Test_Chain33MsgAttributeKey(t *testing.T) {
-	assert.Equal(t, UnsupportedAttributeKey.String(), "unsupported")
-	assert.Equal(t, Chain33Sender.String(), "chain33_sender")
-	assert.Equal(t, EthereumReceiver.String(), "ethereum_receiver")
-	assert.Equal(t, Coin.String(), "amount")
-	assert.Equal(t, TokenContractAddress.String(), "token_contract_address")
 }
