7696aa48b90e160e75a9ad4d00cbe68859e78970
vipwzw <vipwzw@vip.qq.com>
2021-09-15 10:38:58 +0800

    auto ci

--- plugin/dapp/cross2eth/cmd/build/setOfflineToken.sh
+++ plugin/dapp/cross2eth/cmd/build/setOfflineToken.sh
@@ -172,7 +172,7 @@ function MainTest() {
         CLIB="docker exec ${dockerNamePrefix}_ebrelayerb_1 /root/ebcli_A"
         CLIC="docker exec ${dockerNamePrefix}_ebrelayerc_1 /root/ebcli_A"
         CLID="docker exec ${dockerNamePrefix}_ebrelayerd_1 /root/ebcli_A"
-        
+
         docker_ganachetest_ip=$(get_docker_addr "${dockerNamePrefix}_ganachetest_1")
         Boss4xCLI="docker exec ${dockerNamePrefix}_ebrelayera_1 /root/boss4x --rpc_laddr http://${docker_chain33_ip}:8901 --rpc_laddr_ethereum http://${docker_ganachetest_ip}:8545 --paraName user.p.para."
 
