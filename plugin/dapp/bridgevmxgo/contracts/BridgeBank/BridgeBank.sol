c1da9d021f6f5aff839dafeb82d8ac85820b491a
zhengjunhe <hzj@disanbo.com>
2021-09-30 15:38:46 +0800

    add draft sol code for bridgevmxgo

---
+++ plugin/dapp/bridgevmxgo/contracts/BridgeBank/BridgeBank.sol
@@ -0,0 +1,336 @@
+pragma solidity ^0.5.0;
+
+import "./GoAssetBank.sol";
+import "./EvmAssetBank.sol";
+import "../Oracle.sol";
+import "../GoAssetBridge.sol";
+
+/**
+ * @title BridgeBank
+ * @dev Bank contract which coordinates asset-related functionality.
+ *      GoAssetBank manages the minting and burning of tokens which
+ *      represent go contract issued assets, while EvmAssetBank manages
+ *      the locking and unlocking of Chain33 and ERC20 token assets
+ *      based on Chain33.
+ **/
+
+contract BridgeBank is GoAssetBank, EvmAssetBank {
+
+    using SafeMath for uint256;
+    
+    address public operator;
+    Oracle public oracle;
+    GoAssetBridge public goAssetBridge;
+
+    /*
+    * @dev: Constructor, sets operator
+    */
+    constructor (
+        address _operatorAddress,
+        address _oracleAddress,
+        address _goAssetBridgeAddress
+    )
+        public
+    {
+        operator = _operatorAddress;
+        oracle = Oracle(_oracleAddress);
+        goAssetBridge = GoAssetBridge(_goAssetBridgeAddress);
+    }
+
+    /*
+    * @dev: Modifier to restrict access to operator
+    */
+    modifier onlyOperator() {
+        require(
+            msg.sender == operator,
+            'Must be BridgeBank operator.'
+        );
+        _;
+    }
+
+    /*
+     * @dev: Modifier to restrict access to Offline
+     */
+    modifier onlyOffline() {
+        require(
+            msg.sender == offlineSave,
+            'Must be onlyOffline.'
+        );
+        _;
+    }
+
+    /*
+    * @dev: Modifier to restrict access to the oracle
+    */
+    modifier onlyOracle()
+    {
+        require(
+            msg.sender == address(oracle),
+            "Access restricted to the oracle"
+        );
+        _;
+    }
+
+    /*
+    * @dev: Modifier to restrict access to the goAsset bridge
+    */
+    modifier onlyGoAssetBridge()
+    {
+        require(
+            msg.sender == address(goAssetBridge),
+            "Access restricted to the goAsset bridge"
+        );
+        _;
+    }
+
+   /*
+    * @dev: Fallback function allows operator to send funds to the bank directly
+    *       This feature is used for testing and is available at the operator's own risk.
+    */
+    function() external payable onlyOffline {}
+
+    /*
+    * @dev: Creates a new BridgeToken
+    *
+    * @param _symbol: The new BridgeToken's symbol
+    * @return: The new BridgeToken contract's address
+    */
+    function createNewBridgeToken(
+        string memory _symbol
+    )
+        public
+        onlyOperator
+        returns(address)
+    {
+        return deployNewBridgeToken(_symbol);
+    }
+
+    /*
+     * @dev: Mints new BankTokens
+     *
+     * @param _goAssetSender: The goAsset sender's address.
+     * @param _chain33Recipient: The intended recipient's Chain33 address.
+     * @param _bridgeTokenAddress: The bridge token address
+     * @param _symbol: goAsset token symbol
+     * @param _amount: number of goAsset tokens to be minted
+     */
+     function mintBridgeTokens(
+        address  _goAssetSender,
+        address payable _intendedRecipient,
+        address _bridgeTokenAddress,
+        string memory _symbol,
+        uint256 _amount
+    )
+        public
+        onlyGoAssetBridge
+    {
+        return mintNewBridgeTokens(
+            _goAssetSender,
+            _intendedRecipient,
+            _bridgeTokenAddress,
+            _symbol,
+            _amount
+        );
+    }
+
+    /*
+     * @dev: Burns bank tokens
+     *
+     * @param _goAssetReceiver: The _goAsset receiver address in bytes.
+     * @param _goAssetTokenAddress: The currency type
+     * @param _amount: number of goAsset tokens to be burned
+     */
+    function burnBridgeTokens(
+    　　　address  _goAssetReceiver,
+        address _goAssetTokenAddress,
+        uint256 _amount
+    )
+        public
+    {
+        return burnGoAssetTokens(
+            msg.sender,
+            _goAssetReceiver,
+            _goAssetTokenAddress,
+             _amount
+        );
+    }
+
+    /*
+    * @dev: addToken2LockList used to add token with the specified address to be
+    *       allowed locked from GoAsset
+    *
+    * @param _token: token contract address
+    * @param _symbol: token symbol
+    */
+    function addToken2LockList(
+        address _token,
+        string memory _symbol
+    )
+        public
+        onlyOperator
+    {
+        addToken2AllowLock(_token, _symbol);
+    }
+
+   /*
+    * @dev: configTokenOfflineSave used to config threshold to trigger tranfer token to offline account
+    *       when the balance of locked token reaches
+    *
+    * @param _token: token contract address
+    * @param _symbol:token symbol,just used for double check that token address and symbol is consistent
+    * @param _threshold: _threshold to trigger transfer
+    * @param _percents: amount to transfer per percents of threshold
+    */
+    function configLockedTokenOfflineSave(
+        address _token,
+        string memory _symbol,
+        uint256 _threshold,
+        uint8 _percents
+    )
+    public
+    onlyOperator
+    {
+        if (address(0) != _token) {
+            require(keccak256(bytes(BridgeToken(_token).symbol())) == keccak256(bytes(_symbol)), "token address and symbol is not consistent");
+        } else {
+            require(keccak256(bytes("BTY")) == keccak256(bytes(_symbol)), "token address and symbol is not consistent");
+        }
+
+        configOfflineSave4Lock(_token, _symbol, _threshold, _percents);
+    }
+
+   /*
+   * @dev: configOfflineSaveAccount used to config offline account to receive token
+   *       when the balance of locked token reaches threshold
+   *
+   * @param _offlineSave: receiver address
+   */
+    function configOfflineSaveAccount(address payable _offlineSave) public onlyOperator
+    {
+        offlineSave = _offlineSave;
+    }
+
+    /*
+    * @dev: Locks received Chain33 funds.
+    *
+    * @param _recipient: bytes representation of destination address.
+    * @param _token: token address in origin chain (0x0 if chain33)
+    * @param _amount: value of deposit
+    */
+    function lock(
+        bytes memory _recipient,
+        address _token,
+        uint256 _amount
+    )
+        public
+        availableNonce()
+        payable
+    {
+        string memory symbol;
+
+        // Chain33 deposit
+        if (msg.value > 0) {
+          require(
+              _token == address(0),
+              "BTY deposits require the 'token' address to be the null address"
+            );
+          require(
+              msg.value == _amount,
+              "The transactions value must be equal the specified amount(BTY decimal is 8)"
+            );
+
+          // Set the the symbol to BTY
+          symbol = "BTY";
+          // ERC20 deposit
+        } else {
+          require(
+              BridgeToken(_token).transferFrom(msg.sender, address(this), _amount),
+              "Contract token allowances insufficient to complete this lock request"
+          );
+          // Set symbol to the ERC20 token's symbol
+          symbol = BridgeToken(_token).symbol();
+          require(
+              tokenAllow2Lock[keccak256(abi.encodePacked(symbol))] == _token,
+              'The token is not allowed to be locked from Chain33.'
+          );
+        }
+
+        lockFunds(
+            msg.sender,
+            _recipient,
+            _token,
+            symbol,
+            _amount
+        );
+    }
+
+   /*
+    * @dev: Unlocks Chain33 and ERC20 tokens held on the contract.
+    *
+    * @param _recipient: recipient's Chain33 address
+    * @param _token: token contract address
+    * @param _symbol: token symbol
+    * @param _amount: wei amount or ERC20 token count
+\   */
+     function unlock(
+        address payable _recipient,
+        address _token,
+        string memory _symbol,
+        uint256 _amount
+    )
+        public
+        onlyGoAssetBridge
+        hasLockedFunds(
+            _token,
+            _amount
+        )
+        canDeliver(
+            _token,
+            _amount
+        )
+    {
+        unlockFunds(
+            _recipient,
+            _token,
+            _symbol,
+            _amount
+        );
+    }
+
+    /*
+    * @dev: Exposes an item's current status.
+    *
+    * @param _id: The item in question.
+    * @return: Boolean indicating the lock status.
+    */
+    function getGoAssetDepositStatus(
+        bytes32 _id
+    )
+        public
+        view
+        returns(bool)
+    {
+        return isLockedGoAssetDeposit(_id);
+    }
+
+    /*
+    * @dev: Allows access to a GoAsset deposit's information via its unique identifier.
+    *
+    * @param _id: The deposit to be viewed.
+    * @return: Original sender's Chain33 address.
+    * @return: Intended GoAsset recipient's address in bytes.
+    * @return: The lock deposit's currency, denoted by a token address.
+    * @return: The amount locked in the deposit.
+    * @return: The deposit's unique nonce.
+    */
+    function viewGoAssetDeposit(
+        bytes32 _id
+    )
+        public
+        view
+        returns(bytes memory, address payable, address, uint256)
+    {
+        return getGoAssetDeposit(_id);
+    }
+
+}
---
+++ plugin/dapp/bridgevmxgo/contracts/BridgeBank/GoAssetBank.sol
@@ -0,0 +1,384 @@
+pragma solidity ^0.5.0;
+
+import "../../openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./BridgeToken.sol";
+
+/**
+ * @title GoAssetBank
+ * @dev Manages the deployment and minting of ERC20 compatible BridgeTokens
+ *      which represent assets issued by Go contracts.
+ * @dev 为chain33上的go合约发行的资产进行BridgeTokens合约部署，铸币和销毁
+ **/
+
+contract GoAssetBank {
+
+    using SafeMath for uint256;
+
+    uint256 public bridgeTokenCount;
+    mapping(address => bool) public bridgeTokenWhitelist;
+    mapping(bytes32 => bool) public bridgeTokenCreated;
+    mapping(bytes32 => GoAssetDeposit) goAssetDeposits;
+    mapping(bytes32 => GoAssetBurn) goAssetBurns;
+    mapping(address => DepositBurnCount) depositBurnCounts;
+    mapping(bytes32 => address) public token2address;
+
+    struct GoAssetDeposit {
+        address goAssetSender;
+        address payable chain33Recipient;
+        address bridgeTokenAddress;
+        uint256 amount;
+        bool exist;
+        uint256 nonce;
+    }
+
+    struct DepositBurnCount {
+        uint256 depositCount;
+        uint256 burnCount;
+    }
+
+    struct GoAssetBurn {
+        address goAssetSender;
+        address payable chain33Owner;
+        address bridgeTokenAddress;
+        uint256 amount;
+        uint256 nonce;
+    }
+
+    /*
+    * @dev: Event declarations
+    */
+    event LogNewBridgeToken(
+        address _token,
+        string _symbol
+    );
+
+    event LogBridgeTokenMint(
+        address _token,
+        string _symbol,
+        uint256 _amount,
+        address _beneficiary
+    );
+
+    event LogGoAssetTokenBurn(
+        address _token,
+        string _symbol,
+        uint256 _amount,
+        address _ownerFrom,
+        address _goAssetReceiver,
+        uint256 _nonce
+    );
+
+    /*
+     * @dev: Modifier to make sure this symbol not created now
+     */
+     modifier notCreated(string memory _symbol)
+     {
+         require(
+             !hasBridgeTokenCreated(_symbol),
+             "The symbol has been created already"
+         );
+         _;
+     }
+
+     /*
+     * @dev: Modifier to make sure this symbol not created now
+     */
+     modifier created(string memory _symbol)
+     {
+         require(
+             hasBridgeTokenCreated(_symbol),
+             "The symbol has not been created yet"
+         );
+         _;
+     }
+
+    /*
+    * @dev: Constructor, sets bridgeTokenCount
+    */
+    constructor () public {
+        bridgeTokenCount = 0;
+    }
+
+    /*
+    * @dev: check whether this symbol has been created yet or not
+    *
+    * @param _symbol: token symbol
+    * @return: true or false
+    */
+    function hasBridgeTokenCreated(string memory _symbol) public view returns(bool) {
+        bytes32 symHash = keccak256(abi.encodePacked(_symbol));
+        return bridgeTokenCreated[symHash];
+    }
+
+    /*
+    * @dev: Creates a new GoAssetDeposit with a unique ID
+    *
+    * @param _goAssetSender: The _goAssetSender sender's address.
+    * @param _chain33Recipient: The intended recipient's Chain33 address.
+    * @param _token: The currency type
+    * @param _amount: The amount in the deposit.
+    * @return: The newly created GoAssetSenderDeposit's unique id.
+    */
+    function newGoAssetDeposit(
+        address memory _goAssetSender,
+        address payable _chain33Recipient,
+        address _token,
+        uint256 _amount
+    )
+        internal
+        returns(bytes32)
+    {
+        DepositBurnCount memory depositBurnCount = depositBurnCounts[_token];
+        depositBurnCount.depositCount = depositBurnCount.depositCount.add(1);
+        depositBurnCounts[_token] = depositBurnCount;
+
+        bytes32 depositID = keccak256(
+            abi.encodePacked(
+                _goAssetSender,
+                _chain33Recipient,
+                _token,
+                _amount,
+                depositBurnCount.depositCount
+            )
+        );
+
+        goAssetDeposits[depositID] = GoAssetDeposit(
+            _goAssetSender,
+            _chain33Recipient,
+            _token,
+            _amount,
+            true,
+            depositBurnCount.depositCount
+        );
+
+        return depositID;
+    }
+
+    /*
+    * @dev: Creates a new GoAssetBurn with a unique ID
+        *
+        * @param _goAssetSender: The go Asset Sender address
+        * @param _chain33Owner: The owner's Chain33 address.
+        * @param _token: The token Address
+        * @param _amount: The amount to be burned.
+        * @param _nonce: The nonce indicates the burn count for this token
+        * @return: The newly created GoAssetBurn's unique id.
+        */
+        function newGoAssetBurn(
+            address _goAssetSender,
+            address payable _chain33Owner,
+            address _token,
+            uint256 _amount,
+            uint256 nonce
+        )
+            internal
+            returns(bytes32)
+        {
+            bytes32 burnID = keccak256(
+                abi.encodePacked(
+                    _goAssetSender,
+                    _chain33Owner,
+                    _token,
+                    _amount,
+                    nonce
+                )
+            );
+
+            goAssetBurns[burnID] = GoAssetBurn(
+                _goAssetSender,
+                _chain33Owner,
+                _token,
+                _amount,
+                nonce
+            );
+
+            return burnID;
+        }
+
+
+    /*
+     * @dev: Deploys a new BridgeToken contract
+     *
+     * @param _symbol: The BridgeToken's symbol
+     */
+    function deployNewBridgeToken(
+        string memory _symbol
+    )
+        internal
+        notCreated(_symbol)
+        returns(address)
+    {
+        bridgeTokenCount = bridgeTokenCount.add(1);
+
+        // Deploy new bridge token contract
+        BridgeToken newBridgeToken = (new BridgeToken)(_symbol);
+
+        // Set address in tokens mapping
+        address newBridgeTokenAddress = address(newBridgeToken);
+        bridgeTokenWhitelist[newBridgeTokenAddress] = true;
+        bytes32 symHash = keccak256(abi.encodePacked(_symbol));
+        bridgeTokenCreated[symHash] = true;
+        depositBurnCounts[newBridgeTokenAddress] = DepositBurnCount(
+            uint256(0),
+            uint256(0));
+        token2address[symHash] = newBridgeTokenAddress;
+
+        emit LogNewBridgeToken(
+            newBridgeTokenAddress,
+            _symbol
+        );
+
+        return newBridgeTokenAddress;
+    }
+
+    /*
+     * @dev: Mints new goAsset tokens
+     *
+     * @param _goAssetSender:  The _goAssetSender sender's address.
+     * @param _chain33Recipient: The intended recipient's Chain33 address.
+     * @param _goAssetTokenAddress: The currency type
+     * @param _symbol: goAsset token symbol
+     * @param _amount: number of goAsset tokens to be minted
+     */
+     function mintNewBridgeTokens(
+        address  _goAssetSender,
+        address payable _intendedRecipient,
+        address _bridgeTokenAddress,
+        string memory _symbol,
+        uint256 _amount
+    )
+        internal
+    {
+        // Must be whitelisted bridge token
+        require(
+            bridgeTokenWhitelist[_bridgeTokenAddress],
+            "Token must be a whitelisted bridge token"
+        );
+
+        // Mint bridge tokens
+        require(
+            BridgeToken(_bridgeTokenAddress).mint(
+                _intendedRecipient,
+                _amount
+            ),
+            "Attempted mint of bridge tokens failed"
+        );
+
+        newGoAssetDeposit(
+            _goAssetSender,
+            _intendedRecipient,
+            _bridgeTokenAddress,
+            _amount
+        );
+
+        emit LogBridgeTokenMint(
+            _bridgeTokenAddress,
+            _symbol,
+            _amount,
+            _intendedRecipient
+        );
+    }
+
+    /*
+     * @dev: Burn goAsset tokens
+     *
+     * @param _from: The address to be burned from
+     * @param _goAssetReceiver: The receiver's GoAsset address in bytes.
+     * @param _goAssetTokenAddress: The token address of goAsset asset issued on chain33
+     * @param _amount: number of goAsset tokens to be minted
+     */
+    function burnGoAssetTokens(
+        address payable _from,
+        address _goAssetReceiver,
+        address _goAssetTokenAddress,
+        uint256 _amount
+    )
+        internal
+    {
+        // Must be whitelisted bridge token
+        require(
+            bridgeTokenWhitelist[_goAssetTokenAddress],
+            "Token must be a whitelisted bridge token"
+        );
+
+        // burn bridge tokens
+        BridgeToken bridgeTokenInstance = BridgeToken(_goAssetTokenAddress);
+        bridgeTokenInstance.burnFrom(_from, _amount);
+
+        DepositBurnCount memory depositBurnCount = depositBurnCounts[_goAssetTokenAddress];
+        require(
+            depositBurnCount.burnCount + 1 > depositBurnCount.burnCount,
+            "burn nonce is not available"
+        );
+        depositBurnCount.burnCount = depositBurnCount.burnCount.add(1);
+        depositBurnCounts[_goAssetTokenAddress] = depositBurnCount;
+
+        newGoAssetBurn(
+            _goAssetReceiver,
+            _from,
+            _goAssetTokenAddress,
+            _amount,
+            depositBurnCount.burnCount
+        );
+
+        emit LogGoAssetTokenBurn(
+            _goAssetTokenAddress,
+            bridgeTokenInstance.symbol(),
+            _amount,
+            _from,
+            _goAssetReceiver,
+            depositBurnCount.burnCount
+        );
+    }
+
+    /*
+    * @dev: Checks if an individual GoAssetDeposit exists.
+    *
+    * @param _id: The unique GoAssetDeposit's id.
+    * @return: Boolean indicating if the GoAssetDeposit exists in memory.
+    */
+    function isLockedGoAssetDeposit(
+        bytes32 _id
+    )
+        internal
+        view
+        returns(bool)
+    {
+        return(goAssetDeposits[_id].exist);
+    }
+
+  /*
+    * @dev: Gets an item's information
+    *
+    * @param _Id: The item containing the desired information.
+    * @return: Sender's address.
+    * @return: Recipient's address in bytes.
+    * @return: Token address.
+    * @return: Amount of chain33/erc20 in the item.
+    * @return: Unique nonce of the item.
+    */
+    function getGoAssetDeposit(
+        bytes32 _id
+    )
+        internal
+        view
+        returns(bytes memory, address payable, address, uint256)
+    {
+        GoAssetDeposit memory deposit = goAssetDeposits[_id];
+
+        return(
+            deposit.goAssetSender,
+            deposit.chain33Recipient,
+            deposit.bridgeTokenAddress,
+            deposit.amount
+        );
+    }
+
+    function getToken2address(string memory _symbol)
+        created(_symbol)
+        public view returns(address)
+    {
+        bytes32 symHash = keccak256(abi.encodePacked(_symbol));
+        return token2address[symHash];
+    }
+}
