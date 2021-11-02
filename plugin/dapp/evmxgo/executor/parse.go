6da06fced4d83666d2fc30e670ce59dad1f22648
zhengjunhe <hzj@disanbo.com>
2021-11-02 10:00:55 +0800

    correct lock parameter

--- plugin/dapp/evmxgo/executor/parse.go
+++ plugin/dapp/evmxgo/executor/parse.go
@@ -6,6 +6,7 @@ import (
 	"github.com/33cn/chain33/types"
 	bridgevmxgo "github.com/33cn/plugin/plugin/dapp/bridgevmxgo/contracts/generated"
 	chain33Abi "github.com/33cn/plugin/plugin/dapp/evm/executor/abi"
+	evmtypes "github.com/33cn/plugin/plugin/dapp/evm/types"
 	evmxgotypes "github.com/33cn/plugin/plugin/dapp/evmxgo/types"
 )
 
@@ -16,7 +17,12 @@ const (
 //solidity interface: function lock(address _recipient, address _token, uint256 _amount)
 //铸币交易的接收人必须与发起lock交易时填写的接收地址一致
 func checkMinePara(mint *evmxgotypes.EvmxgoMint, tx2lock *types.Transaction) error {
-	unpack, err := chain33Abi.Unpack(tx2lock.Payload, LockMethod, bridgevmxgo.BridgeBankABI)
+	var action evmtypes.EVMContractAction
+	if err := types.Decode(tx2lock.Payload, &action); nil != err {
+		return err
+	}
+
+	unpack, err := chain33Abi.Unpack(action.Para, LockMethod, bridgevmxgo.BridgeBankABI)
 	if err != nil {
 		return err
 	}
