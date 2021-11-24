5f4110934c6db7904725ef1fc72e648719eab5c2
qs <lqs@33.cn>
2021-11-24 18:56:12 +0800

    ci:ci-plugin

--- plugin/dapp/bridgevmxgo/cmd/build/paracrosstestcase.sh
+++ plugin/dapp/bridgevmxgo/cmd/build/paracrosstestcase.sh
@@ -424,7 +424,7 @@ function para_create_nodegroup() {
     ##        exit 1
     #    fi
 
-    echo "=========== # para chain approve node group ============="
+    echo "=========== # bridgevmxgo para chain approve node group ============="
     ##approve
     txhash=$(${PARA_CLI} send para nodegroup approve -i "$id" -c 6 -k 0xc34b5d9d44ac7b754806f761d3d4d2c4fe5214f6b074c19f069c4f5c2a29c8cc)
     echo "tx=$txhash"
