e4911ab8b842bd8ffb0837ed796e880103db1c38
loujiahui <ljh@33.cn>
2022-03-01 19:24:10 +0800

    fix

--- plugin/dapp/zksync/executor/zkproof.go
+++ plugin/dapp/zksync/executor/zkproof.go
@@ -130,7 +130,7 @@ func getLastCommitProofData(db dbm.KV) (*zt.CommitProofState, error) {
 	key := getLastCommitProofKey()
 	v, err := db.Get(key)
 	if err != nil {
-		return nil, types.ErrNotFound
+		return nil, errors.Wrapf(err, "get db")
 	}
 	var data zt.CommitProofState
 	err = types.Decode(v, &data)
