ecd34eb840c34f7a5cefa75bf726f27d25999713
loujiahui <ljh@33.cn>
2022-03-11 17:13:22 +0800

    fix

--- plugin/dapp/zksync/wallet/utils.go
+++ plugin/dapp/zksync/wallet/utils.go
@@ -139,7 +139,7 @@ func GetDepositMsg(payload *zt.ZkDeposit) *zt.ZkMsg {
 	ethAddress, _ := new(big.Int).SetString(strings.ToLower(payload.EthAddress), 16)
 	pubData = append(pubData, getBigEndBitsWithFixLen(ethAddress, zt.AddrBitWidth)...)
 
-	chain33Address, _ := new(big.Int).SetString(payload.Chain33Addr, 16)
+	chain33Address, _ := new(big.Int).SetString(payload.Chain33Addr, 10)
 	pubData = append(pubData, getBigEndBitsWithFixLen(chain33Address, zt.Chain33AddrBitWidth)...)
 
 	copy(binaryData, pubData)
@@ -254,7 +254,7 @@ func GetTransferToNewMsg(payload *zt.ZkTransferToNew) *zt.ZkMsg {
 
 	pubData = append(pubData, getBigEndBitsWithFixLen(ethAddress, zt.AddrBitWidth)...)
 
-	chain33Address, _ := new(big.Int).SetString(payload.ToChain33Address, 16)
+	chain33Address, _ := new(big.Int).SetString(payload.ToChain33Address, 10)
 	pubData = append(pubData, getBigEndBitsWithFixLen(chain33Address, zt.Chain33AddrBitWidth)...)
 
 	copy(binaryData, pubData)
