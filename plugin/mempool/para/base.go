122f1df3f8d50b8407f845f6157c85d4e7fc7005
libangzhu <601276804@qq.com>
2022-06-23 14:00:02 +0800

    plugin mempool 校验nonce

--- plugin/mempool/para/base.go
+++ plugin/mempool/para/base.go
@@ -2,7 +2,11 @@ package para
 
 import (
 	"context"
+	"strings"
 	"sync/atomic"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 
 	"sync"
 
@@ -21,7 +25,8 @@ type Mempool struct {
 	wg          sync.WaitGroup
 	client      queue.Client
 	mainGrpcCli types.Chain33Client
-	isclose     int32
+
+	isclose int32
 }
 
 //NewMempool 新建mempool 实例
@@ -46,6 +51,32 @@ func (mem *Mempool) SetQueueClient(client queue.Client) {
 			case types.EventTx:
 				mlog.Info("Receive msg from para mempool")
 				tx := msg.GetData().(*types.Transaction)
+				if tx.GetSignature().GetTy() == types.EncodeSignID(types.SECP256K1ETH, 2) {
+					//检查NONCE
+					if !strings.Contains(string(tx.Execer), "evm") {
+						continue
+					}
+					ecli, err := ethclient.Dial("http://localhost:8546")
+					if err != nil {
+						log.Error("plugin SetQueueClient", "err", err)
+						continue
+					}
+					nonce, err := ecli.NonceAt(context.Background(), common.HexToAddress(tx.From()), nil)
+					if err != nil {
+						log.Error("NonceAt SetQueueClient", "err", err)
+						continue
+					}
+					if tx.GetNonce() < int64(nonce) {
+						//return fmt.Errorf("nonce too low")
+						msg.Reply(mem.client.NewMessage("rpc", types.EventTx, &types.Reply{IsOk: false, Msg: []byte("nonce too low.")}))
+					}
+
+					if tx.GetNonce() > int64(nonce) {
+						//return &types.Reply{IsOk: false,Msg: []byte{"nonce too height."}} ,fmt.Errorf("nonce too height")
+						msg.Reply(mem.client.NewMessage("rpc", types.EventTx, &types.Reply{IsOk: false, Msg: []byte("nonce too height.")}))
+					}
+				}
+
 				reply, err = mem.mainGrpcCli.SendTransaction(context.Background(), tx)
 			case types.EventGetProperFee:
 				reply, err = mem.mainGrpcCli.GetProperFee(context.Background(), &types.ReqProperFee{})
