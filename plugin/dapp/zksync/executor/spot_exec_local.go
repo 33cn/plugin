9af2c17f9c1b4e27152ac29b60aebb0c44cbfb85
linj <linj@disanbo.com>
2022-08-05 15:43:39 +0800

    both call zksync and spot local

--- plugin/dapp/zksync/executor/spot_exec_local.go
+++ plugin/dapp/zksync/executor/spot_exec_local.go
@@ -11,41 +11,41 @@ import (
  */
 
 func (e *zksync) ExecLocal_LimitOrder(payload *ety.SpotLimitOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
 func (e *zksync) ExecLocal_MarketOrder(payload *ety.SpotMarketOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
 func (e *zksync) ExecLocal_RevokeOrder(payload *ety.SpotRevokeOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
 func (e *zksync) ExecLocal_EntrustOrder(payload *ety.SpotLimitOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
 func (e *zksync) ExecLocal_EntrustRevokeOrder(payload *ety.SpotMarketOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
 func (e *zksync) ExecLocal_SpotNftOrder(payload *ety.SpotNftOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
 func (e *zksync) ExecLocal_SpotNftTradeOrder(payload *ety.SpotNftTakerOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
 func (e *zksync) ExecLocal_SpotNftTradeOrder2(payload *ety.SpotNftTakerOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 func (e *zksync) ExecLocal_SpotNftOrder2(payload *ety.SpotNftOrder, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return e.interExecLocalWithZk(tx, receiptData, index)
+	return e.interExecLocalZksyncSpot(tx, receiptData, index)
 }
 
-func (e *zksync) interExecLocalWithZk(tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+func (e *zksync) interExecLocalZksyncSpot(tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
 	dbSet, err := e.execLocalZksync(tx, receiptData, index)
 	if err != nil {
 		return dbSet, err
