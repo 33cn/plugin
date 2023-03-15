b89c12bf5dcb09ea5a04ab75e888120144ab391c
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2023-03-15 17:58:17 +0800

    update rollup config

--- plugin/dapp/rollup/cmd/build.sh
+++ plugin/dapp/rollup/cmd/build.sh
@@ -8,4 +8,11 @@ OUT_DIR="${1}/$strapp"
 #FLAG=$2
 
 mkdir -p "${OUT_DIR}"
-#cp ./build/* "${OUT_DIR}"
+cp ./ci/* "${OUT_DIR}"
+
+CHAIN33_PATH=$(go list -f "{{.Dir}}" github.com/33cn/chain33)
+PLUGIN_PATH=$(go list -f "{{.Dir}}" github.com/33cn/plugin)
+# copy chain33 toml
+
+cp "${CHAIN33_PATH}/cmd/chain33/chain33.test.toml" "${OUT_DIR}"
+cp "${PLUGIN_PATH}/chain33.para.toml" "${OUT_DIR}"
