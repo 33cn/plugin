d8af8bd363b78c55f1a0d311264e254f66eaab7e
libangzhu <601276804@qq.com>
2023-04-10 17:49:09 +0800

    add evm ci test

--- plugin/dapp/evm/cmd/build.sh
+++ plugin/dapp/evm/cmd/build.sh
@@ -1,9 +1,20 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
+# 官方ci集成脚本
 strpwd=$(pwd)
 strcmd=${strpwd##*dapp/}
 strapp=${strcmd%/cmd*}
 
+OUT_DIR="${1}/$strapp"
+#FLAG=$2
 OUT_TESTDIR="${1}/dapptest/$strapp"
+
 mkdir -p "${OUT_TESTDIR}"
 cp ./test/test-rpc.sh "${OUT_TESTDIR}"
+
+mkdir -p "${OUT_DIR}"
+cp ./ci/* "${OUT_DIR}"
+
+
+PLUGIN_PATH=$(go list -f "{{.Dir}}" github.com/33cn/plugin)
+# copy chain33 toml
+cp "${PLUGIN_PATH}/chain33.test.toml" "${OUT_DIR}"
---
+++ plugin/dapp/evm/cmd/ci/docker-compose.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+# debug mode
+#set -x
+# Exit immediately if a command exits with a non-zero status.
+set -e
+set -o pipefail
+#set -o verbose
+#set -o xtrace
+
+# os: ubuntu18.04 x64
+# first, you must install jq tool of json
+# sudo apt-get install jq
+# sudo apt-get install shellcheck, in order to static check shell script
+# sudo apt-get install parallel
+# ./docker-compose.sh build
+
+PWD=$(cd "$(dirname "$0")" && pwd)
+export PATH="$PWD:$PATH"
+
+SOLO_NODE="${1}_main_1"
+SOLO_CLI="docker exec ${SOLO_NODE} /root/chain33-cli --rpc_laddr http://localhost:8545"
+
+# shellcheck disable=SC2034
+
+containers=("${SOLO_NODE}")
+export COMPOSE_PROJECT_NAME="$1"
+## global config ###
+sedfix=""
+if [ "$(uname)" == "Darwin" ]; then
+    sedfix=".bak"
+fi
+
+echo "=========== # env setting ============="
+echo "COMPOSE_FILE=$COMPOSE_FILE"
+echo "COMPOSE_PROJECT_NAME=$COMPOSE_PROJECT_NAME"
+echo "CLI=SOLO_CLI"
+####################
+#0xd83b69C56834E85e023B1738E69BFA2F0dd52905
+genesisKey="c8729f05b10cc74d40feeb00376e11aa5b50e92b369d778b31b6e902c528f141"
+genesisAddr="0xd83b69C56834E85e023B1738E69BFA2F0dd52905"
+testAddr="0xDe79A84DD3A16BB91044167075dE17a1CA4b1d6b"
+
+function set_main_config() {
+    echo "====== ${FUNCNAME[0]} ======"
+    conf="chain33.test.toml"
+    rm -rf chain33.toml
+    sed -i $sedfix '0,/^Title.*/s//Title="local"/' "${conf}"
+
+    # rpc
+    sed -i $sedfix '0,/^whitelist=.*/s//whitelist=["*"]/' "${conf}"
+    sed -i $sedfix '0,/^httpAddr=.*/s//httpAddr="0.0.0.0:8545"/' "${conf}"
+    #consensus
+    #genesis
+    sed -i $sedfix '0,/^genesis=.*/s//genesis="0xd83b69C56834E85e023B1738E69BFA2F0dd52905"/' "${conf}"
+    sed -i $sedfix '0,/^name=.*/s//name=solo/' "${conf}"
+}
+
+function start_docker() {
+    echo "=========== # docker-compose ps ============="
+    docker-compose ps
+
+    # remove exsit container
+    docker-compose down
+
+    # create and run docker-compose container
+    docker-compose up --build -d
+
+    local SLEEP=5
+    echo "=========== sleep ${SLEEP}s ============="
+    sleep ${SLEEP}
+
+    docker-compose ps
+}
+function check_docker_container() {
+    echo "===== check_docker_container ======"
+    for con in "${containers[@]}"; do
+        runing=$(docker inspect "${con}" | jq '.[0].State.Running')
+        if [ ! "${runing}" ]; then
+            docker inspect "${con}"
+            echo "check ${con} not actived!"
+            exit 1
+        fi
+    done
+}
+
+function testcase_coinsTransfer(){
+    echo "====== ${FUNCNAME[0]} start ======"
+    #coins 转账
+    #构造交易
+    echo "============= create eth tx ============="
+    rawTx=$(${CLI} coins transfer_eth -f ${genesisAddr}  -t ${testAddr} -a 12)
+    echo "${rawTx}"
+     #如果返回空
+     if [ -z "${rawTx}" ]; then
+        exit 1
+     fi
+     echo "============= sign eth tx ============="
+     #签名交易
+    signData=$(${CLI} wallet sign -d ${rawTx} -c 2999 -p 2)
+    #如果返回空
+     if [ -z "${signData}" ]; then
+        exit 1
+     fi
+
+    echo "============= send eth tx ============="
+    hash=$(${CLI} wallet send -d ${signData})
+    if [ -z "${signData}" ]; then
+        exit 1
+    fi
+    echo "${hash}"
+
+    balance=$(${CLI} account balance -a ${testAddr}| jq -r ".balance")
+    if [ "${balance}" != "12.0000" ]; then
+        echo " balance  not correct, balance=${balance}"
+        exit 1
+    fi
+
+}
+
+function testcase_evmPrecompile(){
+  echo "=========== # evmPrecompile ============="
+}
+
+
+function run_tesstcase(){
+  testcase_coinsTransfer
+}
+function main() {
+     echo "====================DAPP=$DAPP main begin==================="
+    ### init config ####
+    echo "#### set main_config"
+    set_main_config
+    ### start docker
+    echo "#### start docker"
+    start_docker
+    run_tesstcase
+    check_docker_container
+    #finish
+    echo "===============DAPP=$DAPP main end==============="
+}
+
+# start
+main
